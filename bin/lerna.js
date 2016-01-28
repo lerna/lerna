06f297b209391e52d4f732b1bd42a79c5b4553fe
James Kyle <me@thejameskyle.com>
2016-01-28 12:22:07 -0800

    Show help when invalid command

--- bin/lerna.js
+++ bin/lerna.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var commands = require("../lib/commands");
+var chalk    = require("chalk");
 var meow     = require("meow");
 var init     = require("../lib/init");
 
@@ -17,6 +18,13 @@ var cli = meow([
 
 var commandName = cli.input[0];
 var command = commands[commandName];
+
+if (!command) {
+  console.log();
+  console.log(chalk.red("  Invalid command: " + chalk.bold(commandName)));
+  cli.showHelp();
+}
+
 var config = init(commandName, process.cwd());
 
 command(config);
