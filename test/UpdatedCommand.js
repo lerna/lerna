72bd9367d8c5585d471b542faab9972a9b4e36b6
Henry Zhu <hi@henryzoo.com>
2016-05-20 21:10:40 -0400

    Add cache for checking updated dependencies (#129)

--- test/UpdatedCommand.js
+++ test/UpdatedCommand.js
@@ -10,113 +10,236 @@ import logger from "../src/logger";
 import stub from "./_stub";
 
 describe("UpdatedCommand", () => {
-  let testDir;
 
-  beforeEach(done => {
-    testDir = initFixture("UpdatedCommand/basic", done);
+  /** =========================================================================
+   * Basic
+   * ======================================================================= */
+
+  describe("Basic", () => {
+    let testDir;
+
+    beforeEach(done => {
+      testDir = initFixture("UpdatedCommand/basic", done);
+    });
+
+    it("should list changes", done => {
+      child.execSync("git tag v1.0.0");
+      child.execSync("touch " + path.join(testDir, "packages/package-2/random-file"));
+      child.execSync("git add -A");
+      child.execSync("git commit -m 'Commit'");
+
+      const updatedCommand = new UpdatedCommand([], {});
+
+      updatedCommand.runValidations();
+      updatedCommand.runPreparations();
+
+      let calls = 0;
+      stub(logger, "info", message => {
+        if (calls === 0) assert.equal(message, "Checking for updated packages...");
+        if (calls === 1) assert.equal(message, "");
+        if (calls === 2) assert.equal(message, "- package-2\n- package-3");
+        if (calls === 3) assert.equal(message, "");
+        calls++;
+      });
+
+      updatedCommand.runCommand(exitWithCode(0, done));
+    });
+
+    it("should list changes with --force-version *", done => {
+      child.execSync("git tag v1.0.0");
+      child.execSync("touch " + path.join(testDir, "packages/package-2/random-file"));
+      child.execSync("git add -A");
+      child.execSync("git commit -m 'Commit'");
+
+      const updatedCommand = new UpdatedCommand([], {
+        forceVersion: "*"
+      });
+
+      updatedCommand.runValidations();
+      updatedCommand.runPreparations();
+
+      let calls = 0;
+      stub(logger, "info", message => {
+        if (calls === 0) assert.equal(message, "Checking for updated packages...");
+        if (calls === 1) assert.equal(message, "");
+        if (calls === 2) assert.equal(message, "- package-1\n- package-2\n- package-3\n- package-4");
+        if (calls === 3) assert.equal(message, "");
+        calls++;
+      });
+
+      updatedCommand.runCommand(exitWithCode(0, done));
+    });
+
+    it("should list changes with --force-version [package,package]", done => {
+      child.execSync("git tag v1.0.0");
+      child.execSync("touch " + path.join(testDir, "packages/package-3/random-file"));
+      child.execSync("git add -A");
+      child.execSync("git commit -m 'Commit'");
+
+      const updatedCommand = new UpdatedCommand([], {
+        forceVersion: "package-2,package-4"
+      });
+
+      updatedCommand.runValidations();
+      updatedCommand.runPreparations();
+
+      let calls = 0;
+      stub(logger, "info", message => {
+        if (calls === 0) assert.equal(message, "Checking for updated packages...");
+        if (calls === 1) assert.equal(message, "");
+        if (calls === 2) assert.equal(message, "- package-2\n- package-3\n- package-4");
+        if (calls === 3) assert.equal(message, "");
+        calls++;
+      });
+
+      updatedCommand.runCommand(exitWithCode(0, done));
+    });
+
+    it("should list changes without ignored files", done => {
+      const lernaJsonLocation = path.join(testDir, "lerna.json");
+      const lernaJson = JSON.parse(fs.readFileSync(lernaJsonLocation));
+      lernaJson.publishConfig = {
+        ignore: ["ignored-file"]
+      };
+      fs.writeFileSync(lernaJsonLocation, JSON.stringify(lernaJson, null, 2));
+
+      child.execSync("git tag v1.0.0");
+      child.execSync("touch " + path.join(testDir, "packages/package-2/ignored-file"));
+      child.execSync("touch " + path.join(testDir, "packages/package-3/random-file"));
+      child.execSync("git add -A");
+      child.execSync("git commit -m 'Commit'");
+
+      const updatedCommand = new UpdatedCommand([], {});
+
+      updatedCommand.runValidations();
+      updatedCommand.runPreparations();
+
+      let calls = 0;
+      stub(logger, "info", message => {
+        if (calls === 0) assert.equal(message, "Checking for updated packages...");
+        if (calls === 1) assert.equal(message, "");
+        if (calls === 2) assert.equal(message, "- package-3");
+        if (calls === 3) assert.equal(message, "");
+        calls++;
+      });
+
+      updatedCommand.runCommand(exitWithCode(0, done));
+    });
   });
 
-  it("should list changes", done => {
-    child.execSync("git tag v1.0.0");
-    child.execSync("touch " + path.join(testDir, "packages/package-2/random-file"));
-    child.execSync("git add -A");
-    child.execSync("git commit -m 'Commit'");
+  /** =========================================================================
+   * Circular
+   * ======================================================================= */
 
-    const updatedCommand = new UpdatedCommand([], {});
+  describe("Circular Dependencies", () => {
+    let testDir;
 
-    updatedCommand.runValidations();
-    updatedCommand.runPreparations();
-
-    let calls = 0;
-    stub(logger, "info", message => {
-      if (calls === 0) assert.equal(message, "Checking for updated packages...");
-      if (calls === 1) assert.equal(message, "");
-      if (calls === 2) assert.equal(message, "- package-2\n- package-3");
-      if (calls === 3) assert.equal(message, "");
-      calls++;
+    beforeEach(done => {
+      testDir = initFixture("UpdatedCommand/circular", done);
     });
 
-    updatedCommand.runCommand(exitWithCode(0, done));
-  });
+    it("should list changes", done => {
+      child.execSync("git tag v1.0.0");
+      child.execSync("touch " + path.join(testDir, "packages/package-3/random-file"));
+      child.execSync("git add -A");
+      child.execSync("git commit -m 'Commit'");
 
-  it("should list changes with --force-version *", done => {
-    child.execSync("git tag v1.0.0");
-    child.execSync("touch " + path.join(testDir, "packages/package-2/random-file"));
-    child.execSync("git add -A");
-    child.execSync("git commit -m 'Commit'");
+      const updatedCommand = new UpdatedCommand([], {});
 
-    const updatedCommand = new UpdatedCommand([], {
-      forceVersion: "*"
+      updatedCommand.runValidations();
+      updatedCommand.runPreparations();
+
+      let calls = 0;
+      stub(logger, "info", message => {
+        if (calls === 0) assert.equal(message, "Checking for updated packages...");
+        if (calls === 1) assert.equal(message, "");
+        if (calls === 2) assert.equal(message, "- package-3\n- package-4");
+        if (calls === 3) assert.equal(message, "");
+        calls++;
+      });
+
+      updatedCommand.runCommand(exitWithCode(0, done));
     });
 
-    updatedCommand.runValidations();
-    updatedCommand.runPreparations();
+    it("should list changes with --force-version *", done => {
+      child.execSync("git tag v1.0.0");
+      child.execSync("touch " + path.join(testDir, "packages/package-2/random-file"));
+      child.execSync("git add -A");
+      child.execSync("git commit -m 'Commit'");
 
-    let calls = 0;
-    stub(logger, "info", message => {
-      if (calls === 0) assert.equal(message, "Checking for updated packages...");
-      if (calls === 1) assert.equal(message, "");
-      if (calls === 2) assert.equal(message, "- package-1\n- package-2\n- package-3\n- package-4");
-      if (calls === 3) assert.equal(message, "");
-      calls++;
+      const updatedCommand = new UpdatedCommand([], {
+        forceVersion: "*"
+      });
+
+      updatedCommand.runValidations();
+      updatedCommand.runPreparations();
+
+      let calls = 0;
+      stub(logger, "info", message => {
+        if (calls === 0) assert.equal(message, "Checking for updated packages...");
+        if (calls === 1) assert.equal(message, "");
+        if (calls === 2) assert.equal(message, "- package-1\n- package-2\n- package-3\n- package-4");
+        if (calls === 3) assert.equal(message, "");
+        calls++;
+      });
+
+      updatedCommand.runCommand(exitWithCode(0, done));
     });
 
-    updatedCommand.runCommand(exitWithCode(0, done));
-  });
+    it("should list changes with --force-version [package,package]", done => {
+      child.execSync("git tag v1.0.0");
+      child.execSync("touch " + path.join(testDir, "packages/package-4/random-file"));
+      child.execSync("git add -A");
+      child.execSync("git commit -m 'Commit'");
 
-  it("should list changes with --force-version [package,package]", done => {
-    child.execSync("git tag v1.0.0");
-    child.execSync("touch " + path.join(testDir, "packages/package-3/random-file"));
-    child.execSync("git add -A");
-    child.execSync("git commit -m 'Commit'");
+      const updatedCommand = new UpdatedCommand([], {
+        forceVersion: "package-2"
+      });
 
-    const updatedCommand = new UpdatedCommand([], {
-      forceVersion: "package-2,package-4"
+      updatedCommand.runValidations();
+      updatedCommand.runPreparations();
+
+      let calls = 0;
+      stub(logger, "info", message => {
+        if (calls === 0) assert.equal(message, "Checking for updated packages...");
+        if (calls === 1) assert.equal(message, "");
+        if (calls === 2) assert.equal(message, "- package-2\n- package-3\n- package-4");
+        if (calls === 3) assert.equal(message, "");
+        calls++;
+      });
+
+      updatedCommand.runCommand(exitWithCode(0, done));
     });
 
-    updatedCommand.runValidations();
-    updatedCommand.runPreparations();
+    it("should list changes without ignored files", done => {
+      const lernaJsonLocation = path.join(testDir, "lerna.json");
+      const lernaJson = JSON.parse(fs.readFileSync(lernaJsonLocation));
+      lernaJson.publishConfig = {
+        ignore: ["ignored-file"]
+      };
+      fs.writeFileSync(lernaJsonLocation, JSON.stringify(lernaJson, null, 2));
 
-    let calls = 0;
-    stub(logger, "info", message => {
-      if (calls === 0) assert.equal(message, "Checking for updated packages...");
-      if (calls === 1) assert.equal(message, "");
-      if (calls === 2) assert.equal(message, "- package-2\n- package-3\n- package-4");
-      if (calls === 3) assert.equal(message, "");
-      calls++;
+      child.execSync("git tag v1.0.0");
+      child.execSync("touch " + path.join(testDir, "packages/package-2/ignored-file"));
+      child.execSync("touch " + path.join(testDir, "packages/package-3/random-file"));
+      child.execSync("git add -A");
+      child.execSync("git commit -m 'Commit'");
+
+      const updatedCommand = new UpdatedCommand([], {});
+
+      updatedCommand.runValidations();
+      updatedCommand.runPreparations();
+
+      let calls = 0;
+      stub(logger, "info", message => {
+        if (calls === 0) assert.equal(message, "Checking for updated packages...");
+        if (calls === 1) assert.equal(message, "");
+        if (calls === 2) assert.equal(message, "- package-3\n- package-4");
+        if (calls === 3) assert.equal(message, "");
+        calls++;
+      });
+
+      updatedCommand.runCommand(exitWithCode(0, done));
     });
-
-    updatedCommand.runCommand(exitWithCode(0, done));
-  });
-
-  it("should list changes without ignored files", done => {
-    const lernaJsonLocation = path.join(testDir, "lerna.json");
-    const lernaJson = JSON.parse(fs.readFileSync(lernaJsonLocation));
-    lernaJson.publishConfig = {
-      ignore: ["ignored-file"]
-    };
-    fs.writeFileSync(lernaJsonLocation, JSON.stringify(lernaJson, null, 2));
-
-    child.execSync("git tag v1.0.0");
-    child.execSync("touch " + path.join(testDir, "packages/package-2/ignored-file"));
-    child.execSync("touch " + path.join(testDir, "packages/package-3/random-file"));
-    child.execSync("git add -A");
-    child.execSync("git commit -m 'Commit'");
-
-    const updatedCommand = new UpdatedCommand([], {});
-
-    updatedCommand.runValidations();
-    updatedCommand.runPreparations();
-
-    let calls = 0;
-    stub(logger, "info", message => {
-      if (calls === 0) assert.equal(message, "Checking for updated packages...");
-      if (calls === 1) assert.equal(message, "");
-      if (calls === 2) assert.equal(message, "- package-3");
-      if (calls === 3) assert.equal(message, "");
-      calls++;
-    });
-
-    updatedCommand.runCommand(exitWithCode(0, done));
   });
 });
