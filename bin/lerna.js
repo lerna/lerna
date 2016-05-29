d5b3c465cd0748e1e2b0cb9e07a2a3a13a970384
Joscha Feth <joscha@feth.com>
2016-05-29 12:27:56 +1000

    style: fix indentation

--- bin/lerna.js
+++ bin/lerna.js
@@ -23,7 +23,7 @@ var cli = meow([
   "  --skip-git           Skip commiting, tagging, and pushing git changes (only affects publish)",
   "  --npm-tag [tagname]  Publish packages with the specified npm dist-tag",
   "  --force-publish      Force publish for the specified packages (comma-separated) or all packages using * (skips the git diff check for changed packages)",
-  "  --yes               Skip all confirmation prompts"
+  "  --yes                Skip all confirmation prompts"
 ], {
   alias: {
     independent: "i",
