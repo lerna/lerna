05da1822750610595e2b1b48c73faf6c884c3cce
Bo Borgerson <gigabo@gmail.com>
2016-06-09 00:51:42 -0700

    Emit partial stderr output from child on maxBuffer exceeded (#181)
    If the error from `child.exec` is just that the child process emitted too much
    on stderr, then that stderr output is likely to be useful.  Emit as much as
    possible, with a warning that it's partial output.

--- src/ChildProcessUtilities.js
+++ src/ChildProcessUtilities.js
@@ -5,6 +5,14 @@ export default class ChildProcessUtilities {
   static exec(command, opts, callback) {
     return child.exec(command, opts, (err, stdout, stderr) => {
       if (err != null) {
+
+        // If the error from `child.exec` is just that the child process
+        // emitted too much on stderr, then that stderr output is likely to
+        // be useful.
+        if (/^stderr maxBuffer exceeded/.test(err.message)) {
+          err = `Error: ${err.message}.  Partial output follows:\n\n${stderr}`;
+        }
+
         callback(err || stderr);
       } else {
         callback(null, stdout);
