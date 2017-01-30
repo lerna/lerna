asini/asini

Automatic hoisting of common dependencies to repo root (#36)
* Resurrect @rygine's groundwork for common dep hoisting

This is taken from the base commit on the "enhanced bootstrap" PR.

* Whitespace for legibility

Easier on my old eyes while I'm trying to grok this. :)

* Update a logger statement

This method was renamed while this change set was in flight.

* Always warn on un-shared dep (even if already installed)

* Only install deps in the root if not already satisfied

* Simplify most common version determination

* Simplify getDependenciesToInstall

Mostly just so I'm sure I understand it.

* Pass dependents back from getDependenciesToInstall

We'll need those to link binaries from the root.

* Actually hoist

* Update commentary

* Link binaries for hosted dependencies

* Don't override an explicit root dependency

* Gate hoisting with --hoist and --nohoist options

* Update a test to expect slight change to install order

* Add a test and fix a bug it uncovered :D

Scoped packages weren't hoisted with `--hoist`.

* Add a test for --nohoist

* Prune hoisted deps from package dirs

* Factor out dependent package fetch

* Add some documentation about hoisting

* Root version may have no dependents

If it's depended on by the root package.json and others don't match.

* Only warn about mismatch with root if ther's a root version

* Start root installs first, since they're likely to take longest

* Make tests expect root install first

* Don't include null deps in root install

These will be present when deps are already satisfied at the root.

* Kill a vestigial comment

src/commands/BootstrapCommand.js
@@ -130,6 +130,182 @@ export default class BootstrapCommand extends Command {
     async.series(actions, callback);
   }
 
+  hoistedDirectory(dependency) {
+    return path.join(this.repository.rootPath, "node_modules", dependency);
+  }
+
+  hoistedPackageJson(dependency) {
+    try {
+      return require(path.join(this.hoistedDirectory(dependency), "package.json"));
+    } catch (e) {
+      // Pass.
+    }
+  }
+
+  /**
+   * Determine if a dependency installed at the root satifies the requirements of the passed packages
+   * This helps to optimize the bootstrap process and skip dependencies that are already installed
+   * @param {String} dependency
+   * @param {Array.<String>} packages
+   */
+  dependencySatisfiesPackages(dependency, packages) {
+    const {version} = (this.hoistedPackageJson(dependency) || {});
+    return packages.every((pkg) => {
+      return semver.satisfies(
+        version,
+        pkg.allDependencies[dependency]
+      );
+    });
+  }
+
+  /**
+   * Given an array of packages, return map of dependencies to install
+   * @param {Array.<Package>} packages An array of packages
+   * @returns {Object}
+   */
+  getDependenciesToInstall(packages = []) {
+
+    // find package by name
+    const findPackage = (name, version) => find(this.packages, (pkg) => {
+      return pkg.name === name && (!version || semver.satisfies(pkg.version, version));
+    });
+
+    const hasPackage = (name, version) => Boolean(findPackage(name, version));
+
+    // Configuration for what packages to hoist may be in asini.json or it may
+    // come in as command line options.
+    const {hoist: scope, nohoist: ignore} = this.getOptions();
+
+    // This will contain entries for each hoistable dependency.
+    const root = [];
+
+    // This will map packages to lists of unhoistable dependencies
+    const leaves = {};
+
+    /**
+     * Map of dependencies to install
+     * {
+     *   <name>: {
+     *     versions: {
+     *       <version>: <# of dependents>
+     *     },
+     *     dependents: {
+     *       <version>: [<dependent1>, <dependent2>, ...]
+     *     }
+     *   }
+     * }
+     *
+     * Example:
+     *
+     * {
+     *   react: {
+     *     versions: {
+     *       "15.x": 3,
+     *       "^0.14.0": 1
+     *     },
+     *     dependents: {
+     *       "15.x": ["my-component1", "my-component2", "my-component3"],
+     *       "^0.14.0": ["my-component4"],
+     *     }
+     *   }
+     * }
+     */
+    const depsToInstall = {};
+
+    // get the map of external dependencies to install
+    packages.forEach((pkg) => {
+
+      // for all package dependencies
+      Object.keys(pkg.allDependencies)
+
+        // map to package or normalized external dependency
+        .map((name) => findPackage(name, pkg.allDependencies[name]) || { name, version: pkg.allDependencies[name] })
+
+        // match external and version mismatched local packages
+        .filter((dep) => !hasPackage(dep.name, dep.version) || !pkg.hasMatchingDependency(dep))
+
+        .forEach(({name, version}) => {
+
+          // Get the object for this package, auto-vivifying.
+          const dep = depsToInstall[name] || (depsToInstall[name] = {
+            versions   : {},
+            dependents : {}
+          });
+
+          // Add this version if it's the first time we've seen it.
+          if (!dep.versions[version]) {
+            dep.versions  [version] = 0;
+            dep.dependents[version] = [];
+          }
+
+          // Record the dependency on this version.
+          dep.versions  [version]++;
+          dep.dependents[version].push(pkg.name);
+        });
+    });
+
+    // determine where each dependency will be installed
+    Object.keys(depsToInstall).forEach((name) => {
+      const {versions, dependents} = depsToInstall[name];
+
+      let rootVersion;
+
+      if (scope && PackageUtilities.getFilteredPackage({name}, {scope, ignore})) {
+
+        // Get the most common version.
+        const commonVersion = Object.keys(versions)
+          .reduce((a, b) => versions[a] > versions[b] ? a : b);
+
+        // Get the version required by the repo root (if any).
+        // If the root doesn't have a dependency on this package then we'll
+        // install the most common dependency there.
+        rootVersion = this.repository.package.allDependencies[name] || commonVersion;
+
+        if (rootVersion !== commonVersion) {
+          this.logger.warn(
+            `The repository root depends on ${name}@${rootVersion}, ` +
+            `which differs from the more common ${name}@${commonVersion}.`
+          );
+        }
+
+        // Install the best version we can in the repo root.
+        // Even if it's already installed there we still need to make sure any
+        // binaries are linked to the packages that depend on them.
+        root.push({
+          name,
+          dependents: (dependents[rootVersion] || [])
+            .map((dep) => this.packageGraph.get(dep).package),
+          dependency: this.repository.hasDependencyInstalled(name, rootVersion)
+            ? null // Don't re-install if it's already there.
+            : `${name}@${rootVersion}`,
+        });
+      }
+
+      // Add less common versions to package installs.
+      Object.keys(versions).forEach((version) => {
+
+        // Only install deps that can't be hoisted in the leaves.
+        if (version === rootVersion) return;
+
+        dependents[version].forEach((pkg) => {
+
+          if (rootVersion) {
+            this.logger.warn(
+              `"${pkg}" package depends on ${name}@${version}, ` +
+              `which differs from the hoisted ${name}@${rootVersion}.`
+            );
+          }
+
+          // only install dependency if it's not already installed
+          if (!findPackage(pkg).hasDependencyInstalled(name)) {
+            (leaves[pkg] || (leaves[pkg] = [])).push(`${name}@${version}`);
+          }
+        });
+      });
+    });
+    return { root, leaves };
+  }
+
   /**
    * Determine if a dependency installed at the root satifies the requirements of the passed packages
    * This helps to optimize the bootstrap process and skip dependencies that are already installed
@@ -305,27 +481,69 @@ export default class BootstrapCommand extends Command {
    * @param {Function} callback
    */
   installExternalDependencies(callback) {
-    this.logger.info("Installing external dependencies");
-    this.progressBar.init(this.filteredPackages.length);
+    const {leaves, root} = this.getDependenciesToInstall(this.filteredPackages);
+
     const actions = [];
-    this.filteredPackages.forEach((pkg) => {
-      const allDependencies = pkg.allDependencies;
-      const externalPackages = Object.keys(allDependencies)
-        .filter((dependency) => {
-          const match = find(this.packages, (pkg) => {
-            return pkg.name === dependency;
+
+    // Start root install first, if any, since it's likely to take the longest.
+    if (Object.keys(root).length) {
+      actions.push((cb) => NpmUtilities.installInDir(
+        this.repository.rootPath,
+        root.map(({dependency}) => dependency).filter((dep) => dep),
+        (err) => {
+          if (err) return cb(err);
+
+          // Link binaries into dependent packages so npm scripts will have
+          // access to them.
+          async.series(root.map(({name, dependents}) => (cb) => {
+            const {bin} = (this.hoistedPackageJson(name) || {});
+            if (bin) {
+              async.series(dependents.map((pkg) => (cb) => {
+                const src  = this.hoistedDirectory(name);
+                const dest = pkg.nodeModulesLocation;
+                this.createBinaryLink(src, dest, name, bin, cb);
+              }), cb);
+            } else {
+              cb();
+            }
+          }), (err) => {
+            this.progressBar.tick("Install hoisted");
+            cb(err);
           });
-          return !(match && pkg.hasMatchingDependency(match));
-        })
-        .filter((dependency) => !pkg.hasDependencyInstalled(dependency))
-        .map((dependency) => dependency + "@" + allDependencies[dependency]);
-      if (externalPackages.length) {
-        actions.push((cb) => NpmUtilities.installInDir(pkg.location, externalPackages, (err) => {
+        }
+      ));
+
+      // Remove any hoisted dependencies that may have previously been
+      // installed in package directories.
+      actions.push((cb) => {
+        async.series(root.map(({name, dependents}) => (cb) => {
+          async.series(dependents.map(({nodeModulesLocation: dir}) => (cb) => {
+            FileSystemUtilities.rimraf(path.join(dir, name), cb);
+          }), cb);
+        }), (err) => {
+          this.progressBar.tick("Prune hoisted");
+          cb(err);
+        });
+      });
+    }
+
+    // Install anything that needs to go into the leaves.
+    Object.keys(leaves)
+      .map((pkgName) => ({pkg: this.packageGraph.get(pkgName).package, deps: leaves[pkgName]}))
+      .forEach(({pkg, deps}) => actions.push(
+        (cb) => NpmUtilities.installInDir(pkg.location, deps, (err) => {
           this.progressBar.tick(pkg.name);
           cb(err);
-        }));
-      }
-    });
+        })
+      ));
+
+    if (actions.length) {
+
+      this.logger.info("Installing external dependencies");
+
+      this.progressBar.init(actions.length);
+    }
+
     async.parallelLimit(actions, this.concurrency, (err) => {
       this.progressBar.terminate();
       callback(err);

lerna/lerna

src/commands/BootstrapCommand.js
@@ -127,6 +127,182 @@ export default class BootstrapCommand extends Command {
     async.series(actions, callback);
   }
 
+  hoistedDirectory(dependency) {
+    return path.join(this.repository.rootPath, "node_modules", dependency);
+  }
+
+  hoistedPackageJson(dependency) {
+    try {
+      return require(path.join(this.hoistedDirectory(dependency), "package.json"));
+    } catch (e) {
+      // Pass.
+    }
+  }
+
+  /**
+   * Determine if a dependency installed at the root satifies the requirements of the passed packages
+   * This helps to optimize the bootstrap process and skip dependencies that are already installed
+   * @param {String} dependency
+   * @param {Array.<String>} packages
+   */
+  dependencySatisfiesPackages(dependency, packages) {
+    const {version} = (this.hoistedPackageJson(dependency) || {});
+    return packages.every((pkg) => {
+      return semver.satisfies(
+        version,
+        pkg.allDependencies[dependency]
+      );
+    });
+  }
+
+  /**
+   * Given an array of packages, return map of dependencies to install
+   * @param {Array.<Package>} packages An array of packages
+   * @returns {Object}
+   */
+  getDependenciesToInstall(packages = []) {
+
+    // find package by name
+    const findPackage = (name, version) => find(this.packages, (pkg) => {
+      return pkg.name === name && (!version || semver.satisfies(pkg.version, version));
+    });
+
+    const hasPackage = (name, version) => Boolean(findPackage(name, version));
+
+    // Configuration for what packages to hoist may be in lerna.json or it may
+    // come in as command line options.
+    const {hoist: scope, nohoist: ignore} = this.getOptions();
+
+    // This will contain entries for each hoistable dependency.
+    const root = [];
+
+    // This will map packages to lists of unhoistable dependencies
+    const leaves = {};
+
+    /**
+     * Map of dependencies to install
+     * {
+     *   <name>: {
+     *     versions: {
+     *       <version>: <# of dependents>
+     *     },
+     *     dependents: {
+     *       <version>: [<dependent1>, <dependent2>, ...]
+     *     }
+     *   }
+     * }
+     *
+     * Example:
+     *
+     * {
+     *   react: {
+     *     versions: {
+     *       "15.x": 3,
+     *       "^0.14.0": 1
+     *     },
+     *     dependents: {
+     *       "15.x": ["my-component1", "my-component2", "my-component3"],
+     *       "^0.14.0": ["my-component4"],
+     *     }
+     *   }
+     * }
+     */
+    const depsToInstall = {};
+
+    // get the map of external dependencies to install
+    packages.forEach((pkg) => {
+
+      // for all package dependencies
+      Object.keys(pkg.allDependencies)
+
+        // map to package or normalized external dependency
+        .map((name) => findPackage(name, pkg.allDependencies[name]) || { name, version: pkg.allDependencies[name] })
+
+        // match external and version mismatched local packages
+        .filter((dep) => !hasPackage(dep.name, dep.version) || !pkg.hasMatchingDependency(dep))
+
+        .forEach(({name, version}) => {
+
+          // Get the object for this package, auto-vivifying.
+          const dep = depsToInstall[name] || (depsToInstall[name] = {
+            versions   : {},
+            dependents : {}
+          });
+
+          // Add this version if it's the first time we've seen it.
+          if (!dep.versions[version]) {
+            dep.versions  [version] = 0;
+            dep.dependents[version] = [];
+          }
+
+          // Record the dependency on this version.
+          dep.versions  [version]++;
+          dep.dependents[version].push(pkg.name);
+        });
+    });
+
+    // determine where each dependency will be installed
+    Object.keys(depsToInstall).forEach((name) => {
+      const {versions, dependents} = depsToInstall[name];
+
+      let rootVersion;
+
+      if (scope && PackageUtilities.getFilteredPackage({name}, {scope, ignore})) {
+
+        // Get the most common version.
+        const commonVersion = Object.keys(versions)
+          .reduce((a, b) => versions[a] > versions[b] ? a : b);
+
+        // Get the version required by the repo root (if any).
+        // If the root doesn't have a dependency on this package then we'll
+        // install the most common dependency there.
+        rootVersion = this.repository.package.allDependencies[name] || commonVersion;
+
+        if (rootVersion !== commonVersion) {
+          this.logger.warn(
+            `The repository root depends on ${name}@${rootVersion}, ` +
+            `which differs from the more common ${name}@${commonVersion}.`
+          );
+        }
+
+        // Install the best version we can in the repo root.
+        // Even if it's already installed there we still need to make sure any
+        // binaries are linked to the packages that depend on them.
+        root.push({
+          name,
+          dependents: (dependents[rootVersion] || [])
+            .map((dep) => this.packageGraph.get(dep).package),
+          dependency: this.repository.hasDependencyInstalled(name, rootVersion)
+            ? null // Don't re-install if it's already there.
+            : `${name}@${rootVersion}`,
+        });
+      }
+
+      // Add less common versions to package installs.
+      Object.keys(versions).forEach((version) => {
+
+        // Only install deps that can't be hoisted in the leaves.
+        if (version === rootVersion) return;
+
+        dependents[version].forEach((pkg) => {
+
+          if (rootVersion) {
+            this.logger.warn(
+              `"${pkg}" package depends on ${name}@${version}, ` +
+              `which differs from the hoisted ${name}@${rootVersion}.`
+            );
+          }
+
+          // only install dependency if it's not already installed
+          if (!findPackage(pkg).hasDependencyInstalled(name)) {
+            (leaves[pkg] || (leaves[pkg] = [])).push(`${name}@${version}`);
+          }
+        });
+      });
+    });
+    return { root, leaves };
+  }
+
   /**
    * Determine if a dependency installed at the root satifies the requirements of the passed packages
    * This helps to optimize the bootstrap process and skip dependencies that are already installed
@@ -302,27 +478,69 @@ export default class BootstrapCommand extends Command {
    * @param {Function} callback
    */
   installExternalDependencies(callback) {
-    this.logger.info("Installing external dependencies");
-    this.progressBar.init(this.packagesToBootstrap.length);
+    const {leaves, root} = this.getDependenciesToInstall(this.packagesToBootstrap);
+
     const actions = [];
-    this.packagesToBootstrap.forEach((pkg) => {
-      const allDependencies = pkg.allDependencies;
-      const externalPackages = Object.keys(allDependencies)
-        .filter((dependency) => {
-          const match = find(this.packages, (pkg) => {
-            return pkg.name === dependency;
+
+    // Start root install first, if any, since it's likely to take the longest.
+    if (Object.keys(root).length) {
+      actions.push((cb) => NpmUtilities.installInDir(
+        this.repository.rootPath,
+        root.map(({dependency}) => dependency).filter((dep) => dep),
+        (err) => {
+          if (err) return cb(err);
+
+          // Link binaries into dependent packages so npm scripts will have
+          // access to them.
+          async.series(root.map(({name, dependents}) => (cb) => {
+            const {bin} = (this.hoistedPackageJson(name) || {});
+            if (bin) {
+              async.series(dependents.map((pkg) => (cb) => {
+                const src  = this.hoistedDirectory(name);
+                const dest = pkg.nodeModulesLocation;
+                this.createBinaryLink(src, dest, name, bin, cb);
+              }), cb);
+            } else {
+              cb();
+            }
+          }), (err) => {
+            this.progressBar.tick("Install hoisted");
+            cb(err);
           });
-          return !(match && pkg.hasMatchingDependency(match));
-        })
-        .filter((dependency) => !pkg.hasDependencyInstalled(dependency))
-        .map((dependency) => dependency + "@" + allDependencies[dependency]);
-      if (externalPackages.length) {
-        actions.push((cb) => NpmUtilities.installInDir(pkg.location, externalPackages, (err) => {
+        }
+      ));
+
+      // Remove any hoisted dependencies that may have previously been
+      // installed in package directories.
+      actions.push((cb) => {
+        async.series(root.map(({name, dependents}) => (cb) => {
+          async.series(dependents.map(({nodeModulesLocation: dir}) => (cb) => {
+            FileSystemUtilities.rimraf(path.join(dir, name), cb);
+          }), cb);
+        }), (err) => {
+          this.progressBar.tick("Prune hoisted");
+          cb(err);
+        });
+      });
+    }
+
+    // Install anything that needs to go into the leaves.
+    Object.keys(leaves)
+      .map((pkgName) => ({pkg: this.packageGraph.get(pkgName).package, deps: leaves[pkgName]}))
+      .forEach(({pkg, deps}) => actions.push(
+        (cb) => NpmUtilities.installInDir(pkg.location, deps, (err) => {
           this.progressBar.tick(pkg.name);
           cb(err);
-        }));
-      }
-    });
+        })
+      ));
+
+    if (actions.length) {
+
+      this.logger.info("Installing external dependencies");
+
+      this.progressBar.init(actions.length);
+    }
+
     async.parallelLimit(actions, this.concurrency, (err) => {
       this.progressBar.terminate();
       callback(err);