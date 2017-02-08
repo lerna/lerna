91869998c70a4a51f6a62d7411dc8cfd6718c961
Simon Andrews <me@simonster.net>
2017-02-08 21:30:50 +0000

    Always run test and env scripts
    Fixes #491
    
    npm defines default scripts for `test` and `env`. Lerna only runs
    scripts when they’re defined in `package.json`, but this could lead to
    unexpected behaviour in these two cases. In particular, it could lead
    to `pretest` and `posttest` not being run by Lerna, when npm would have
    run them.

--- src/commands/RunCommand.js
+++ src/commands/RunCommand.js
@@ -12,8 +12,12 @@ export default class RunCommand extends Command {
       return;
     }
 
-    this.packagesWithScript = this.filteredPackages
-      .filter((pkg) => pkg.scripts && pkg.scripts[this.script]);
+    if (this.script === "test" || this.script === "env") {
+      this.packagesWithScript = this.packages;
+    } else {
+      this.packagesWithScript = this.filteredPackages
+        .filter((pkg) => pkg.scripts && pkg.scripts[this.script]);
+    }
 
     if (!this.packagesWithScript.length) {
       callback(new Error(`No packages found with the npm script '${this.script}'`));
