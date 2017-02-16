925c0a1b5faeab66d221bc9a6d6b51f10b99f185
Ryan Tsao <ryan.j.tsao@gmail.com>
2017-02-16 12:46:26 -0800

    Fix --ignore flag when globs are expanded to an array (#601)
    Clarifies the logic when negating the glob.

--- src/PackageUtilities.js
+++ src/PackageUtilities.js
@@ -140,9 +140,11 @@ export default class PackageUtilities {
 
     if (!Array.isArray(glob)) glob = [glob];
 
-    const maybeNegate = negate ? (v) => !v : (v) => v;
-
-    return glob.some((glob) => maybeNegate(minimatch(pkg.name, glob)));
+    if (negate) {
+      return glob.every((glob) => !minimatch(pkg.name, glob));
+    } else {
+      return glob.some((glob) => minimatch(pkg.name, glob));
+    }
   }
 
   static getFilteredPackage(pkg, {scope, ignore}) {
