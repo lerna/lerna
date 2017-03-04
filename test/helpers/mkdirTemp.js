70ba79b9fe4dcd5fb915030ebc6669baa2a27c65
Daniel Stockman <daniels@zillowgroup.com>
2017-03-03 17:36:30 -0800

    mkdirTemp: filter dirPath result with realpath to avoid symlink jank
    Node uses realpathSync when require()ing, which breaks when TMPDIR is a symlink.

--- test/helpers/mkdirTemp.js
+++ test/helpers/mkdirTemp.js
@@ -1,17 +1,13 @@
-import fs from "fs";
+import fs from "graceful-fs";
 import os from "os";
 import path from "path";
+import pify from "pify";
+
+const mkdtempAsync = pify(fs.mkdtemp);
+const realpathAsync = pify(fs.realpath);
 
 export default function mkdirTemp(cwd) {
   const prefix = path.join(cwd || os.tmpdir(), path.sep);
-
-  return new Promise((resolve, reject) => {
-    fs.mkdtemp(prefix, (err, dirPath) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(dirPath);
-    });
-  });
+  // realpath because OS X symlinks /var -> /private/var
+  return mkdtempAsync(prefix).then(realpathAsync);
 }
