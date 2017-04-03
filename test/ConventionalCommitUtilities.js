eecc09934770f9954e1f4256a3c95e242c8bd0f4
Daniel Stockman <daniels@zillowgroup.com>
2017-04-03 14:14:48 -0700

    test/ConventionalCommitUtilities: use jest.mock() in unit tests

--- test/ConventionalCommitUtilities.js
+++ test/ConventionalCommitUtilities.js
@@ -1,115 +1,95 @@
-import assert from "assert";
 import dedent from "dedent";
-import assertStubbedCalls from "./helpers/assertStubbedCalls";
 import path from "path";
 
-import ConventionalCommitUtilities from "../src/ConventionalCommitUtilities";
+// mocked modules
 import ChildProcessUtilities from "../src/ChildProcessUtilities";
 import FileSystemUtilities from "../src/FileSystemUtilities";
 
+// file under test
+import ConventionalCommitUtilities from "../src/ConventionalCommitUtilities";
+
+jest.mock("../src/ChildProcessUtilities");
+jest.mock("../src/FileSystemUtilities");
+
 describe("ConventionalCommitUtilities", () => {
+  afterEach(() => jest.resetAllMocks());
+
   describe(".recommendVersion()", () => {
     it("should invoke conventional-changelog-recommended bump to fetch next version", () => {
-      assertStubbedCalls([
-        [ChildProcessUtilities, "execSync", {}, [
-          {
-            args: [require.resolve("conventional-recommended-bump/cli.js") + " -l bar --commit-path=/foo/bar -p angular"],
-            returns: "major"
-          }
-        ]],
-      ]);
+      ChildProcessUtilities.execSync = jest.fn(() => "major");
 
-      const recommendedVersion = ConventionalCommitUtilities.recommendVersion({
+      const recommendVersion = ConventionalCommitUtilities.recommendVersion({
         name: "bar",
         version: "1.0.0",
-        location: "/foo/bar"
+        location: "/foo/bar",
       });
 
-      assert.equal(recommendedVersion, "2.0.0");
+      expect(recommendVersion).toBe("2.0.0");
+      expect(ChildProcessUtilities.execSync).lastCalledWith(
+        `${require.resolve("conventional-recommended-bump/cli.js")} -l bar --commit-path=/foo/bar -p angular`,
+      );
     });
   });
 
   describe(".updateChangelog()", () => {
     it("should populate initial CHANGELOG.md if it does not exist", () => {
-      assertStubbedCalls([
-        [FileSystemUtilities, "existsSync", {}, [
-          {
-            args: [path.normalize("/foo/bar/CHANGELOG.md")],
-            returns: false
-          }
-        ]],
-        [ChildProcessUtilities, "execSync", {}, [
-          {
-            args: [`${require.resolve("conventional-changelog-cli/cli.js")} -l bar --commit-path=/foo/bar --pkg=${path.normalize("/foo/bar/package.json")} -p angular`],
-            returns: "<a name='change' />feat: I should be placed in the CHANGELOG"
-          }
-        ]],
-        [FileSystemUtilities, "writeFileSync", {}, [
-          {
-            args: [path.normalize("/foo/bar/CHANGELOG.md"), dedent(`
-            # Change Log
-
-            All notable changes to this project will be documented in this file.
-            See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
-
-            <a name='change' />feat: I should be placed in the CHANGELOG`)],
-            returns: null
-          }
-        ]]
-      ]);
+      FileSystemUtilities.existsSync = jest.fn(() => false);
+      ChildProcessUtilities.execSync = jest.fn(() => "<a name='change' />feat: I should be placed in the CHANGELOG");
 
       ConventionalCommitUtilities.updateChangelog({
         name: "bar",
         location: "/foo/bar"
       });
+
+      expect(FileSystemUtilities.existsSync).lastCalledWith(
+        path.normalize("/foo/bar/CHANGELOG.md")
+      );
+      expect(ChildProcessUtilities.execSync).lastCalledWith(
+        `${require.resolve("conventional-changelog-cli/cli.js")} -l bar --commit-path=/foo/bar --pkg=${path.normalize("/foo/bar/package.json")} -p angular`,
+      );
+      expect(FileSystemUtilities.writeFileSync).lastCalledWith(
+        path.normalize("/foo/bar/CHANGELOG.md"),
+        dedent`
+          # Change Log
+
+          All notable changes to this project will be documented in this file.
+          See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+          <a name='change' />feat: I should be placed in the CHANGELOG
+        `
+      );
     });
 
     it("should insert into existing CHANGELOG.md", () => {
-      assertStubbedCalls([
-        [FileSystemUtilities, "existsSync", {}, [
-          {
-            args: [path.normalize("/foo/bar/CHANGELOG.md")],
-            returns: true
-          }
-        ]],
-        [FileSystemUtilities, "readFileSync", {}, [
-          {
-            args: [path.normalize("/foo/bar/CHANGELOG.md")],
-            returns: dedent(`
-            # Change Log
+      FileSystemUtilities.existsSync = jest.fn(() => true);
+      ChildProcessUtilities.execSync = jest.fn(() => "<a name='change2' />fix: a second commit for our CHANGELOG");
+      FileSystemUtilities.readFileSync = jest.fn(() => dedent`
+        # Change Log
 
-            All notable changes to this project will be documented in this file.
-            See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+        All notable changes to this project will be documented in this file.
+        See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
-            <a name='change' />feat: I should be placed in the CHANGELOG`)
-          }
-        ]],
-        [ChildProcessUtilities, "execSync", {}, [
-          {
-            args: [`${require.resolve("conventional-changelog-cli/cli.js")} -l bar --commit-path=/foo/bar/ --pkg=${path.normalize("/foo/bar/package.json")} -p angular`],
-            returns: "<a name='change2' />fix: a second commit for our CHANGELOG"
-          }
-        ]],
-        [FileSystemUtilities, "writeFileSync", {}, [
-          {
-            args: [path.normalize("/foo/bar/CHANGELOG.md"), dedent(`
-            # Change Log
-
-            All notable changes to this project will be documented in this file.
-            See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
-
-            <a name='change2' />fix: a second commit for our CHANGELOG
-
-            <a name='change' />feat: I should be placed in the CHANGELOG`)],
-            returns: null
-          }
-        ]]
-      ]);
+        <a name='change' />feat: I should be placed in the CHANGELOG
+      `);
 
       ConventionalCommitUtilities.updateChangelog({
         name: "bar",
-        location: "/foo/bar/"
+        location: "/foo/bar/",
       });
+
+      expect(FileSystemUtilities.writeFileSync).lastCalledWith(
+        path.normalize("/foo/bar/CHANGELOG.md"),
+        dedent`
+          # Change Log
+
+          All notable changes to this project will be documented in this file.
+          See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+          <a name='change2' />fix: a second commit for our CHANGELOG
+
+          <a name='change' />feat: I should be placed in the CHANGELOG
+        `
+      );
     });
   });
 });
