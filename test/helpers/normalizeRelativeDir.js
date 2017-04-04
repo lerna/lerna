30fe4572e7208067c338e441cab3adc1d765fa3a
Daniel Stockman <daniels@zillowgroup.com>
2017-04-04 14:07:12 -0700

    test/helpers: add normalizeRelativeDir()

---
+++ test/helpers/normalizeRelativeDir.js
@@ -0,0 +1,8 @@
+import path from "path";
+import normalizePath from "normalize-path";
+
+export default function normalizeRelativeDir(testDir, filePath) {
+  return normalizePath(
+    path.relative(testDir, filePath)
+  );
+}
