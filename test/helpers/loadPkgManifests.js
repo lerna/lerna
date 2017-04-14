189110a6cc9306aa0a4cfd612f752671465c0e06
Daniel Stockman <daniels@zillowgroup.com>
2017-04-14 11:48:01 -0700

    test/helpers: add loadPkgManifests()

---
+++ test/helpers/loadPkgManifests.js
@@ -0,0 +1,18 @@
+import globby from "globby";
+import loadJsonFile from "load-json-file";
+
+export default function loadPkgManifests(cwd) {
+  return globby([
+    // all child packages, at any level
+    "**/package.json",
+    // but not the root
+    "!package.json",
+    // and not installed
+    "!**/node_modules",
+  ], {
+    cwd,
+    absolute: true,
+  }).then((files) => Promise.all(
+    files.map((fp) => loadJsonFile(fp))
+  ));
+}
