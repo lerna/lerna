b20b8360341b51271fb4b3db0a25cb2b9c95b16d
Daniel Stockman <daniel.stockman@gmail.com>
2017-04-18 00:28:59 -0700

    test/integration/lerna-publish: async/await

--- test/integration/lerna-publish.test.js
+++ test/integration/lerna-publish.test.js
@@ -8,49 +8,45 @@ const lastCommitMessage = (cwd) =>
   execa.stdout("git", ["log", "-1", "--format=%B"], { cwd }).then(normalizeNewline);
 
 describe("lerna publish", () => {
-  test.concurrent("updates fixed versions", () => {
-    return initFixture("PublishCommand/normal").then((cwd) => {
-      const args = [
-        "publish",
-        "--skip-npm",
-        "--cd-version=patch",
-        "--yes",
-      ];
+  test.concurrent("updates fixed versions", async () => {
+    const cwd = await initFixture("PublishCommand/normal");
+    const args = [
+      "publish",
+      "--skip-npm",
+      "--cd-version=patch",
+      "--yes",
+    ];
 
-      return execa(LERNA_BIN, args, { cwd }).then((result) => {
-        expect(result.stdout).toMatchSnapshot("stdout: updates fixed versions");
+    const stdout = await execa.stdout(LERNA_BIN, args, { cwd });
+    expect(stdout).toMatchSnapshot("stdout: updates fixed versions");
 
-        return Promise.all([
-          loadPkgManifests(cwd),
-          lastCommitMessage(cwd),
-        ]);
-      }).then(([allPackageJsons, commitMessage]) => {
-        expect(allPackageJsons).toMatchSnapshot("packages: updates fixed versions");
-        expect(commitMessage).toMatchSnapshot("commit: updates fixed versions");
-      });
-    });
+    const [allPackageJsons, commitMessage] = await Promise.all([
+      loadPkgManifests(cwd),
+      lastCommitMessage(cwd),
+    ]);
+
+    expect(allPackageJsons).toMatchSnapshot("packages: updates fixed versions");
+    expect(commitMessage).toMatchSnapshot("commit: updates fixed versions");
   });
 
-  test.concurrent("updates independent versions", () => {
-    return initFixture("PublishCommand/independent").then((cwd) => {
-      const args = [
-        "publish",
-        "--skip-npm",
-        "--cd-version=major",
-        "--yes",
-      ];
+  test.concurrent("updates independent versions", async () => {
+    const cwd = await initFixture("PublishCommand/independent");
+    const args = [
+      "publish",
+      "--skip-npm",
+      "--cd-version=major",
+      "--yes",
+    ];
 
-      return execa(LERNA_BIN, args, { cwd }).then((result) => {
-        expect(result.stdout).toMatchSnapshot("stdout: updates independent versions");
+    const stdout = await execa.stdout(LERNA_BIN, args, { cwd });
+    expect(stdout).toMatchSnapshot("stdout: updates independent versions");
 
-        return Promise.all([
-          loadPkgManifests(cwd),
-          lastCommitMessage(cwd),
-        ]);
-      }).then(([allPackageJsons, commitMessage]) => {
-        expect(allPackageJsons).toMatchSnapshot("packages: updates independent versions");
-        expect(commitMessage).toMatchSnapshot("commit: updates independent versions");
-      });
-    });
+    const [allPackageJsons, commitMessage] = await Promise.all([
+      loadPkgManifests(cwd),
+      lastCommitMessage(cwd),
+    ]);
+
+    expect(allPackageJsons).toMatchSnapshot("packages: updates independent versions");
+    expect(commitMessage).toMatchSnapshot("commit: updates independent versions");
   });
 });
