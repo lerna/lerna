3e16fe702fb76a0bbb07e28764f1e7ca56a5973d
Daniel Stockman <daniels@zillowgroup.com>
2017-04-20 08:43:39 -0700

    test/integration/lerna-run: async await

--- test/integration/lerna-run.test.js
+++ test/integration/lerna-run.test.js
@@ -3,40 +3,34 @@ import initFixture from "../helpers/initFixture";
 import { LERNA_BIN } from "../helpers/constants";
 
 describe("lerna run", () => {
-  test.concurrent("my-script --scope", () => {
-    return initFixture("RunCommand/basic").then((cwd) => {
-      const args = [
-        "run",
-        "my-script",
-        "--scope=package-1",
-        "--concurrency=1",
-        // args below tell npm to be quiet
-        "--", "--silent",
-      ];
-
-      return execa(LERNA_BIN, args, { cwd }).then((result) => {
-        expect(result.stdout).toMatchSnapshot("stdout: my-script --scope");
-        expect(result.stderr).toMatchSnapshot("stderr: my-script --scope");
-      });
-    });
+  test.concurrent("my-script --scope", async () => {
+    const cwd = await initFixture("RunCommand/basic");
+    const args = [
+      "run",
+      "my-script",
+      "--scope=package-1",
+      "--concurrency=1",
+      // args below tell npm to be quiet
+      "--", "--silent",
+    ];
+    const { stdout, stderr } = await execa(LERNA_BIN, args, { cwd });
+    expect(stdout).toMatchSnapshot("stdout: my-script --scope");
+    expect(stderr).toMatchSnapshot("stderr: my-script --scope");
   });
 
-  test.concurrent("test --ignore", () => {
-    return initFixture("RunCommand/integration-lifecycle").then((cwd) => {
-      const args = [
-        "run",
-        "--concurrency=1",
-        "test",
-        "--ignore",
-        "package-@(1|2|3)",
-        // args below tell npm to be quiet
-        "--", "--silent",
-      ];
-
-      return execa(LERNA_BIN, args, { cwd }).then((result) => {
-        expect(result.stdout).toMatchSnapshot("stdout: test --ignore");
-        expect(result.stderr).toMatchSnapshot("stderr: test --ignore");
-      });
-    });
+  test.concurrent("test --ignore", async () => {
+    const cwd = await initFixture("RunCommand/integration-lifecycle");
+    const args = [
+      "run",
+      "--concurrency=1",
+      "test",
+      "--ignore",
+      "package-@(1|2|3)",
+      // args below tell npm to be quiet
+      "--", "--silent",
+    ];
+    const { stdout, stderr } = await execa(LERNA_BIN, args, { cwd });
+    expect(stdout).toMatchSnapshot("stdout: test --ignore");
+    expect(stderr).toMatchSnapshot("stderr: test --ignore");
   });
 });
