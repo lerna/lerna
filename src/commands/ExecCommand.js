ef20497b5db71b034580ed03ab0bee1588931a1d
yisraelx <yisraelx@gmail.com>
2017-06-09 15:51:43 +0300

    feat: add $LERNA_ROOT_PATH environmental variable for lerna exec
    To make it easy to run a script that located in the root dir, in a complicated dir structurean, when
    there is no fixed pattern for the distance of the sub-dir level from the root dir.

--- src/commands/ExecCommand.js
+++ src/commands/ExecCommand.js
@@ -82,6 +82,7 @@ export default class ExecCommand extends Command {
       shell: true,
       env: Object.assign({}, process.env, {
         LERNA_PACKAGE_NAME: pkg.name,
+        LERNA_ROOT_PATH: process.cwd(),
       }),
       reject: this.options.bail
     };
