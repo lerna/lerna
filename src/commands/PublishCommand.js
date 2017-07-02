7cdabcc9a8effb0f2679a68f516ae34ad8453a0d
Craig Michael Thompson <craig@craigsworks.com>
2017-07-02 20:57:02 +0100

    feat(canary): Enhanced canary option functionality
    Currently the `canary` option has a few limitations:
    * Hard codes an `alpha` meta suffix on the resulting version
    * Uses the current version, not the next minor version as would be expected in other tools (GitVersion et. al)
    
    This change overloads the `canary` flag, allowing it to be passeda value that will be used for the meta tag instead i.e. `--canary=beta`, which results in a `x.x.x-beta-<sha>` version tag.
    
    BREAKING CHANGES: Now bumps to the next `minor` version instead of using current version

--- src/commands/PublishCommand.js
+++ src/commands/PublishCommand.js
@@ -29,6 +29,10 @@ export const builder = {
     group: "Command Options:",
     describe: "Publish packages after every successful merge using the sha as part of the tag.",
     alias: "c",
+    requiresArg: false,
+    coerce: (canary) => {
+      return canary === true ? "canary" : canary;
+    }
   },
   "cd-version": {
     group: "Command Options:",
@@ -267,7 +271,7 @@ export default class PublishCommand extends Command {
 
     // Non-Independent Canary Mode
     if (!this.repository.isIndependent() && this.options.canary) {
-      const version = this.globalVersion + this.getCanaryVersionSuffix();
+      const version = this.getCanaryVersion(this.globalVersion, this.options.canary);
       callback(null, { version });
 
     // Non-Independent Non-Canary Mode
@@ -283,10 +287,12 @@ export default class PublishCommand extends Command {
     // Independent Canary Mode
     } else if (this.options.canary) {
       const versions = {};
-      const canaryVersionSuffix = this.getCanaryVersionSuffix();
 
       this.updates.forEach((update) => {
-        versions[update.package.name] = update.package.version + canaryVersionSuffix;
+        versions[update.package.name] = this.getCanaryVersion(
+          update.package.version,
+          this.options.canary
+        );
       });
 
       callback(null, { versions });
@@ -321,8 +327,10 @@ export default class PublishCommand extends Command {
     }
   }
 
-  getCanaryVersionSuffix() {
-    return "-alpha." + GitUtilities.getCurrentSHA(this.execOpts).slice(0, 8);
+  getCanaryVersion(version, metaName) {
+    const minor = semver.inc(version, "minor");
+    const hash = GitUtilities.getCurrentSHA(this.execOpts).slice(0, 8);
+    return `${minor}-${metaName}.${hash}`;
   }
 
   promptVersion(packageName, currentVersion, callback) {
