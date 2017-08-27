4c1b8752c32cd0dbc5b737882c6b752f8c65b7fb
Daniel Stockman <daniel.stockman@gmail.com>
2017-08-26 22:53:30 -0700

    yargsRunner: run prettier with the right indent setting

--- test/helpers/yargsRunner.js
+++ test/helpers/yargsRunner.js
@@ -9,31 +9,31 @@ import { builder as globalOptions } from '../../src/Command';
  * @return {Function} with partially-applied yargs config
  */
 export default function yargsRunner(commandModule) {
-    const cmd = commandModule.command.split(' ')[0];
+  const cmd = commandModule.command.split(' ')[0];
 
-    return cwd => {
-        // create a _new_ yargs instance every time cwd changes to avoid singleton pollution
-        const cli = yargs([], cwd).options(globalOptions).command(commandModule);
+  return cwd => {
+    // create a _new_ yargs instance every time cwd changes to avoid singleton pollution
+    const cli = yargs([], cwd).options(globalOptions).command(commandModule);
 
-        return (...args) =>
-            new Promise((resolve, reject) => {
-                const yargsMeta = {};
-                const _onFinish = result => {
-                    Object.assign(result, yargsMeta);
-                    // tests expect errors thrown to indicate failure,
-                    // _not_ just non-zero exitCode
-                    if (result instanceof Error) {
-                        reject(result);
-                    } else {
-                        resolve(result);
-                    }
-                };
-                const context = { _cwd: cwd, _onFinish };
-                const parseFn = (exitError, parsedArgv, yargsOutput) => {
-                    Object.assign(yargsMeta, { exitError, parsedArgv, yargsOutput });
-                };
+    return (...args) =>
+      new Promise((resolve, reject) => {
+        const yargsMeta = {};
+        const _onFinish = result => {
+          Object.assign(result, yargsMeta);
+          // tests expect errors thrown to indicate failure,
+          // _not_ just non-zero exitCode
+          if (result instanceof Error) {
+            reject(result);
+          } else {
+            resolve(result);
+          }
+        };
+        const context = { _cwd: cwd, _onFinish };
+        const parseFn = (yargsError, parsedArgv, yargsOutput) => {
+          Object.assign(yargsMeta, { parsedArgv, yargsOutput });
+        };
 
-                cli.parse([cmd, ...args], context, parseFn);
-            });
-    };
+        cli.parse([cmd, ...args], context, parseFn);
+      });
+  };
 }
