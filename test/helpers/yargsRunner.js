32cb1b36fe964418825d21f059c648cbe2b31a54
Daniel Stockman <daniel.stockman@gmail.com>
2017-08-26 19:11:57 -0700

    add test/helpers/yargsRunner

---
+++ test/helpers/yargsRunner.js
@@ -0,0 +1,39 @@
+import yargs from 'yargs/yargs';
+import { builder as globalOptions } from '../../src/Command';
+
+/**
+ * A higher-order function to help with passing _actual_ yargs-parsed argv
+ * into command constructors (instead of artificial direct parameters).
+ *
+ * @param {Object} commandModule The yargs command exports
+ * @return {Function} with partially-applied yargs config
+ */
+export default function yargsRunner(commandModule) {
+    const cmd = commandModule.command.split(' ')[0];
+
+    return cwd => {
+        // create a _new_ yargs instance every time cwd changes to avoid singleton pollution
+        const cli = yargs([], cwd).options(globalOptions).command(commandModule);
+
+        return (...args) =>
+            new Promise((resolve, reject) => {
+                const yargsMeta = {};
+                const _onFinish = result => {
+                    Object.assign(result, yargsMeta);
+                    // tests expect errors thrown to indicate failure,
+                    // _not_ just non-zero exitCode
+                    if (result instanceof Error) {
+                        reject(result);
+                    } else {
+                        resolve(result);
+                    }
+                };
+                const context = { _cwd: cwd, _onFinish };
+                const parseFn = (exitError, parsedArgv, yargsOutput) => {
+                    Object.assign(yargsMeta, { exitError, parsedArgv, yargsOutput });
+                };
+
+                cli.parse([cmd, ...args], context, parseFn);
+            });
+    };
+}
