6ac03cd174bd477abca29ee59e15acf4fc571d38
Daniel Stockman <daniel.stockman@gmail.com>
2017-08-26 19:21:59 -0700

    DiffCommand: migrate tests to yargsRunner

--- test/DiffCommand.js
+++ test/DiffCommand.js
@@ -7,11 +7,13 @@ import GitUtilities from "../src/GitUtilities";
 
 // helpers
 import callsBack from "./helpers/callsBack";
-import exitWithCode from "./helpers/exitWithCode";
 import initFixture from "./helpers/initFixture";
+import yargsRunner from "./helpers/yargsRunner";
 
 // file under test
-import DiffCommand from "../src/commands/DiffCommand";
+import * as commandModule from "../src/commands/DiffCommand";
+
+const run = yargsRunner(commandModule);
 
 jest.mock("../src/ChildProcessUtilities");
 jest.mock("../src/GitUtilities");
@@ -26,158 +28,107 @@ describe("DiffCommand", () => {
 
   beforeEach(() => initFixture("DiffCommand/basic").then((dir) => {
     testDir = dir;
+    GitUtilities.isInitialized.mockImplementation(() => true);
     GitUtilities.hasCommit.mockImplementation(() => true);
   }));
   afterEach(() => jest.resetAllMocks());
 
-  it("should diff everything from the first commit", (done) => {
+  it("should diff everything from the first commit", () => {
     GitUtilities.getFirstCommit.mockImplementation(() => "beefcafe");
     ChildProcessUtilities.spawn.mockImplementation(callbackSuccess);
 
-    const diffCommand = new DiffCommand([], {}, testDir);
-
-    diffCommand.runValidations();
-    diffCommand.runPreparations();
-
-    diffCommand.runCommand(exitWithCode(0, (err) => {
-      if (err) return done.fail(err);
-      try {
-        expect(ChildProcessUtilities.spawn).lastCalledWith(
-          "git",
-          [
-            "diff",
-            "beefcafe",
-            "--color=auto",
-          ],
-          expect.objectContaining({
-            cwd: testDir,
-          }),
-          expect.any(Function)
-        );
-        done();
-      } catch (ex) {
-        done.fail(ex);
-      }
-    }));
+    return run(testDir)().then(() => {
+      expect(ChildProcessUtilities.spawn).lastCalledWith(
+        "git",
+        [
+          "diff",
+          "beefcafe",
+          "--color=auto",
+        ],
+        expect.objectContaining({
+          cwd: testDir,
+        }),
+        expect.any(Function)
+      );
+    });
   });
 
-  it("should diff everything from the most recent tag", (done) => {
+  it("should diff everything from the most recent tag", () => {
     GitUtilities.hasTags.mockImplementation(() => true);
     GitUtilities.getLastTaggedCommit.mockImplementation(() => "cafedead");
     ChildProcessUtilities.spawn.mockImplementation(callbackSuccess);
 
-    const diffCommand = new DiffCommand([], {}, testDir);
-
-    diffCommand.runValidations();
-    diffCommand.runPreparations();
-
-    diffCommand.runCommand(exitWithCode(0, (err) => {
-      if (err) return done.fail(err);
-      try {
-        expect(ChildProcessUtilities.spawn).lastCalledWith(
-          "git",
-          [
-            "diff",
-            "cafedead",
-            "--color=auto",
-          ],
-          expect.objectContaining({
-            cwd: testDir,
-          }),
-          expect.any(Function)
-        );
-        done();
-      } catch (ex) {
-        done.fail(ex);
-      }
-    }));
+    return run(testDir)().then(() => {
+      expect(ChildProcessUtilities.spawn).lastCalledWith(
+        "git",
+        [
+          "diff",
+          "cafedead",
+          "--color=auto",
+        ],
+        expect.objectContaining({
+          cwd: testDir,
+        }),
+        expect.any(Function)
+      );
+    });
   });
 
-  it("should diff a specific package", (done) => {
+  it("should diff a specific package", () => {
     GitUtilities.getFirstCommit.mockImplementation(() => "deadbeef");
     ChildProcessUtilities.spawn.mockImplementation(callbackSuccess);
 
-    const diffCommand = new DiffCommand(["package-1"], {}, testDir);
-
-    diffCommand.runValidations();
-    diffCommand.runPreparations();
-
-    diffCommand.runCommand(exitWithCode(0, (err) => {
-      if (err) return done.fail(err);
-      try {
-        expect(ChildProcessUtilities.spawn).lastCalledWith(
-          "git",
-          [
-            "diff",
-            "deadbeef",
-            "--color=auto",
-            "--",
-            path.join(testDir, "packages/package-1"),
-          ],
-          expect.objectContaining({
-            cwd: testDir,
-          }),
-          expect.any(Function)
-        );
-        done();
-      } catch (ex) {
-        done.fail(ex);
-      }
-    }));
+    return run(testDir)(
+      "package-1"
+    ).then(() => {
+      expect(ChildProcessUtilities.spawn).lastCalledWith(
+        "git",
+        [
+          "diff",
+          "deadbeef",
+          "--color=auto",
+          "--",
+          path.join(testDir, "packages/package-1"),
+        ],
+        expect.objectContaining({
+          cwd: testDir,
+        }),
+        expect.any(Function)
+      );
+    });
   });
 
-  it("should error when attempting to diff a package that doesn't exist", (done) => {
-    const diffCommand = new DiffCommand(["missing"], {}, testDir);
-
-    diffCommand.runValidations();
-    diffCommand.runPreparations();
-
-    diffCommand.runCommand(exitWithCode(1, (err) => {
-      try {
-        expect(err.message).toBe("Package 'missing' does not exist.");
-        done();
-      } catch (ex) {
-        done.fail(ex);
-      }
-    }));
+  it("should error when attempting to diff a package that doesn't exist", () => {
+    return run(testDir)(
+      "missing"
+    ).catch((err) => {
+      expect(err.exitCode).toBe(1);
+      expect(err.message).toBe("Package 'missing' does not exist.");
+    });
   });
 
-  it("should error when running in a repository without commits", (done) => {
+  it("should error when running in a repository without commits", () => {
     // override beforeEach mock
     GitUtilities.hasCommit.mockImplementation(() => false);
 
-    const diffCommand = new DiffCommand(["package-1"], {}, testDir);
-
-    diffCommand.runValidations();
-    diffCommand.runPreparations();
-
-    diffCommand.runCommand(exitWithCode(1, (err) => {
-      try {
-        expect(err.message).toBe("Can't diff. There are no commits in this repository, yet.");
-        done();
-      } catch (ex) {
-        done.fail(ex);
-      }
-    }));
+    return run(testDir)(
+      "package-1"
+    ).catch((err) => {
+      expect(err.exitCode).toBe(1);
+      expect(err.message).toBe("Can't diff. There are no commits in this repository, yet.");
+    });
   });
 
-  it("should error when git diff exits non-zero", (done) => {
+  it("should error when git diff exits non-zero", () => {
     const err = new Error("An actual non-zero, not git diff pager SIGPIPE");
     err.code = 1;
     ChildProcessUtilities.spawn.mockImplementation(callsBack(err));
 
-    const diffCommand = new DiffCommand(["package-1"], {}, testDir);
-
-    diffCommand.runValidations();
-    diffCommand.runPreparations();
-
-    diffCommand.runCommand(exitWithCode(1, (err) => {
-      try {
-        expect(err.message).toBe("An actual non-zero, not git diff pager SIGPIPE");
-        done();
-      } catch (ex) {
-        done.fail(ex);
-      }
-    }));
+    return run(testDir)(
+      "package-1"
+    ).catch((err) => {
+      expect(err.exitCode).toBe(1);
+      expect(err.message).toBe("An actual non-zero, not git diff pager SIGPIPE");
+    });
   });
 });
