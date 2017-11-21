ff6dbf5e6916836a7fb8ffe478226637b2d77951
Mario Nebl <hello@mario-nebl.de>
2017-11-21 22:25:45 +0100

    revert: remove remnants of #1122

--- packages/cli/cli.js
+++ packages/cli/cli.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
 "use strict";
-const importLocal = require("import-local");
 
-if (importLocal(__filename)) {
-  require("npmlog").verbose("cli", "using local version of lerna");
-} else {
-  require("@lerna/core/lib/cli")().parse(process.argv.slice(2));
-}
-
+require("@lerna/core/lib/cli")().parse(process.argv.slice(2));
