c07012b32bf735e0b2d3b49cf1a059bced458e69
Daniel Stockman <daniel.stockman@gmail.com>
2018-01-10 10:31:17 -0800

    lint: fix no-console

--- src/Command.js
+++ src/Command.js
@@ -468,12 +468,12 @@ export default class Command {
     const pkgPrefix = `${err.cmd} [${err.pkg.name}]`;
     log.error(pkgPrefix, `Output from stdout:`);
     log.pause();
-    console.error(err.stdout);
+    console.error(err.stdout); // eslint-disable-line no-console
 
     log.resume();
     log.error(pkgPrefix, `Output from stderr:`);
     log.pause();
-    console.error(err.stderr);
+    console.error(err.stderr); // eslint-disable-line no-console
 
     // Below is just to ensure something sensible is printed after the long
     // stream of logs
