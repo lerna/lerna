abe51704b9a0b312bbc1da6f94f5e0ca6fb9c4b6
Daniel Stockman <daniel.stockman@gmail.com>
2018-01-12 11:40:49 -0800

    convert(cjs): utils/splitVersion

--- src/utils/splitVersion.js
+++ src/utils/splitVersion.js
@@ -1,7 +1,11 @@
+"use strict";
+
+module.exports = splitVersion;
+
 // Take a dep like "foo@^1.0.0".
 // Return a tuple like ["foo", "^1.0.0"].
 // Handles scoped packages.
 // Returns undefined for version if none specified.
-export default function splitVersion(dep) {
+function splitVersion(dep) {
   return dep.match(/^(@?[^@]+)(?:@(.+))?/).slice(1, 3);
 }
