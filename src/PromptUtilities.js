801094f05f13619d3f62c13fc30829268c1377aa
Daniel Stockman <daniel.stockman@gmail.com>
2018-01-12 11:40:49 -0800

    convert(cjs): PromptUtilities

--- src/PromptUtilities.js
+++ src/PromptUtilities.js
@@ -1,60 +1,64 @@
-import inquirer from "inquirer";
-import log from "npmlog";
+"use strict";
 
-export default class PromptUtilities {
-  static confirm(message, callback) {
-    log.pause();
-    inquirer
-      .prompt([
-        {
-          type: "expand",
-          name: "confirm",
-          message,
-          default: 2, // default to help in order to avoid clicking straight through
-          choices: [{ key: "y", name: "Yes", value: true }, { key: "n", name: "No", value: false }],
-        },
-      ])
-      .then(answers => {
-        log.resume();
-        callback(answers.confirm);
-      });
-  }
+const inquirer = require("inquirer");
+const log = require("npmlog");
 
-  static select(message, { choices, filter, validate } = {}, callback) {
-    log.pause();
-    inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "prompt",
-          message,
-          choices,
-          pageSize: choices.length,
-          filter,
-          validate,
-        },
-      ])
-      .then(answers => {
-        log.resume();
-        callback(answers.prompt);
-      });
-  }
+exports.confirm = confirm;
+exports.select = select;
+exports.input = input;
 
-  static input(message, { filter, validate } = {}, callback) {
-    log.pause();
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "input",
-          message,
-          filter,
-          validate,
-        },
-      ])
-      .then(answers => {
-        log.resume();
-        callback(answers.input);
-      });
-  }
+function confirm(message, callback) {
+  log.pause();
+  inquirer
+    .prompt([
+      {
+        type: "expand",
+        name: "confirm",
+        message,
+        default: 2, // default to help in order to avoid clicking straight through
+        choices: [{ key: "y", name: "Yes", value: true }, { key: "n", name: "No", value: false }],
+      },
+    ])
+    .then(answers => {
+      log.resume();
+      callback(answers.confirm);
+    });
+}
+
+function select(message, { choices, filter, validate } = {}, callback) {
+  log.pause();
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "prompt",
+        message,
+        choices,
+        pageSize: choices.length,
+        filter,
+        validate,
+      },
+    ])
+    .then(answers => {
+      log.resume();
+      callback(answers.prompt);
+    });
+}
+
+function input(message, { filter, validate } = {}, callback) {
+  log.pause();
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "input",
+        message,
+        filter,
+        validate,
+      },
+    ])
+    .then(answers => {
+      log.resume();
+      callback(answers.input);
+    });
 }
