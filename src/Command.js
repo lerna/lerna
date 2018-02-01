21998aa4dd760dd34c4d4b299749d0665c27babc
Fritz <fritz-c@users.noreply.github.com>
2018-02-01 17:01:35 +0900

    fix: add other potential cause of git initialization failure

--- src/Command.js
+++ src/Command.js
@@ -230,7 +230,7 @@ class Command {
     if (this.requiresGit && !GitUtilities.isInitialized(this.execOpts)) {
       throw new ValidationError(
         "ENOGIT",
-        "This is not a git repository, did you already run `git init` or `lerna init`?"
+        "git binary missing, or this is not a git repository. Did you already run `git init` or `lerna init`?"
       );
     }
 
