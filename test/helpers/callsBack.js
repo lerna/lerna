e9f30643f620cc2b0606d5ef895643b626a4699c
Daniel Stockman <daniel.stockman@gmail.com>
2018-02-11 15:41:34 -0800

    chore(test/helpers): ensure callsBack() waits for the next tick

--- test/helpers/callsBack.js
+++ test/helpers/callsBack.js
@@ -4,5 +4,5 @@ module.exports = callsBack;
 
 // for mocking the behaviour of methods that accept a callback
 function callsBack(err, val) {
-  return (...args) => args.pop()(err, val);
+  return (...args) => process.nextTick(args.pop(), err, val);
 }
