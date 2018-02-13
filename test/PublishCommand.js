lerna/lerna

commit a560b73d499a11d0fce03c07de9dfecb482dd878

PackageGraph is a Map, its nodes store relationship Sets (#1260)
A lot of existing code was re-calculating stuff that should live inside the PackageGraph; namely, the relationships between packages (localDependencies and localDependents). I made PackageGraph a subclass of Map, mostly because it already had a `.get()` method, and now it can be iterated and such. It's a lot more elegant, overall.

A big piece of the new relationship logic is based on [`npm-package-arg`](https://github.com/npm/npm-package-arg#result-object) parsing, which resolves a lot of previously half-baked logic. It's no longer necessary to pass the git version parser to the graph constructor, for example. `npm-package-arg` will also facilitate future graph manipulations, such as recognizing relative `file:` specifiers correctly.

### `Package`

* The package `bin` field now properly resolves an object when the original value is a string shorthand.
* Many `Package` properties are immutable now, and very few are actually enumerable.

### `PackageGraph`

* `--force-local` evaluation has been moved into `PackageGraph`'s constructor, simplifying the existing evaluation of local dependencies during symlink creation in `LinkCommand`.
* Cycle detection and removal has been moved into a `PackageGraph` instance method, making the topological batching algogrithm much easier to read.
* `PackageGraphNode`s no longer have a confusingly-named "dependencies" list, but distinct `Map` instances `.localDependencies` and `.localDependents`.
* `PackageGraphNode`s themselves now forward most of the interesting `Package` properties, so the potential is there to use them directly, instead of the myriad places we pluck a (renamed) `pkg` property from various containers.

### `UpdatedPackagesCollector`

* Now reuses all of the new graph calculation in `PackageGraph` instead of blithely duplicating it.
* There's a lot of room for other improvements in this utility.

### `PackageUtilities`

src/UpdatedPackagesCollector.js
@@ -13,7 +13,7 @@ class Update {
   }
 }
 
-function getForcedPackages({ forcePublish }) {
+function getForcedPackages(forcePublish) {
   // new Set(null) is equivalent to new Set([])
   // i.e., an empty Set
   let inputs = null;
@@ -34,171 +34,10 @@ function getForcedPackages({ forcePublish }) {
   return new Set(inputs);
 }
 
-class UpdatedPackagesCollector {
-  constructor(command) {
-    this.execOpts = command.execOpts;
-    this.logger = command.logger;
-    this.repository = command.repository;
-    this.packages = command.filteredPackages;
-    this.packageGraph = command.packageGraph;
-    this.options = command.options;
-  }
-
-  getUpdates() {
-    this.logger.silly("getUpdates");
-
-    this.updatedPackages = this.collectUpdatedPackages();
-    this.prereleasedPackages = this.collectPrereleasedPackages();
-    this.dependents = this.collectDependents();
-    return this.collectUpdates();
-  }
-
-  collectUpdatedPackages() {
-    this.logger.info("", "Checking for updated packages...");
-
-    const { execOpts, options } = this;
-    const { canary } = options;
-    let { since } = options;
-
-    if (GitUtilities.hasTags(execOpts)) {
-      if (canary) {
-        const currentSHA = GitUtilities.getCurrentSHA(execOpts);
-
-        since = this.getAssociatedCommits(currentSHA);
-      } else if (!since) {
-        since = GitUtilities.getLastTag(execOpts);
-      }
-    }
-
-    this.logger.info("", `Comparing with ${since || "initial commit"}.`);
-
-    const updatedPackages = {};
-
-    const registerUpdated = pkg => {
-      this.logger.verbose("updated", pkg.name);
-      updatedPackages[pkg.name] = pkg;
-    };
-
-    const forced = getForcedPackages(options);
-
-    if (!since || forced.has("*")) {
-      this.packages.forEach(registerUpdated);
-    } else {
-      this.packages
-        .filter(pkg => {
-          if (forced.has(pkg.name)) {
-            return true;
-          }
-          return this.hasDiffSinceThatIsntIgnored(pkg, since);
-        })
-        .forEach(registerUpdated);
-    }
-
-    return updatedPackages;
-  }
-
-  isPackageDependentOf(packageName, dependency) {
-    this.logger.silly("isPackageDependentOf", packageName, dependency);
-
-    if (!this.cache[packageName]) {
-      this.cache[packageName] = {};
-    }
-
-    if (this.cache[packageName][dependency] === "dependent") {
-      return true;
-    } else if (this.cache[packageName][dependency] === "visited") {
-      return false;
-    }
-
-    const graphDependencies = this.packageGraph.get(packageName).dependencies;
-
-    if (graphDependencies.indexOf(dependency) > -1) {
-      this.cache[packageName][dependency] = "dependent";
-      return true;
-    }
-
-    this.cache[packageName][dependency] = "visited";
-
-    let hasSubDependents = false;
-
-    graphDependencies.forEach(dep => {
-      if (this.isPackageDependentOf(dep, dependency)) {
-        this.cache[packageName][dependency] = "dependent";
-        hasSubDependents = true;
-      }
-    });
-
-    return hasSubDependents;
-  }
-
-  collectDependents() {
-    this.logger.silly("collectDependents");
-
-    const dependents = {};
-    this.cache = {};
-    const keys = Object.keys(Object.assign({}, this.updatedPackages, this.prereleasedPackages));
-
-    this.packages.forEach(pkg => {
-      keys.forEach(dependency => {
-        if (this.isPackageDependentOf(pkg.name, dependency)) {
-          this.logger.verbose("dependent", "%s depends on %s", pkg.name, dependency);
-          dependents[pkg.name] = pkg;
-        }
-      });
-    });
-
-    return dependents;
-  }
-
-  isPrereleaseIncrement() {
-    const { cdVersion } = this.options;
-    return cdVersion && cdVersion.startsWith("pre");
-  }
-
-  collectPrereleasedPackages() {
-    this.logger.info("", "Checking for prereleased packages...");
-    if (this.isPrereleaseIncrement()) {
-      return {};
-    }
-
-    const prereleasedPackages = {};
-
-    this.packages.forEach(pkg => {
-      if (semver.prerelease(pkg.version)) {
-        this.logger.verbose("prereleased", pkg.name);
-        prereleasedPackages[pkg.name] = pkg;
-      }
-    });
-
-    return prereleasedPackages;
-  }
-
-  collectUpdates() {
-    this.logger.silly("collectUpdates");
-
-    return this.packages
-      .filter(
-        pkg =>
-          this.updatedPackages[pkg.name] ||
-          this.prereleasedPackages[pkg.name] ||
-          this.dependents[pkg.name] ||
-          this.options.canary
-      )
-      .map(pkg => {
-        this.logger.verbose("has filtered update", pkg.name);
-        return new Update(pkg);
-      });
-  }
-
-  getAssociatedCommits(sha) {
-    // if it's a merge commit, it will return all the commits that were part of the merge
-    // ex: If `ab7533e` had 2 commits, ab7533e^..ab7533e would contain 2 commits + the merge commit
-    return `${sha.slice(0, 8)}^..${sha.slice(0, 8)}`;
-  }
-
-  hasDiffSinceThatIsntIgnored(pkg, commits) {
-    const folder = path.relative(this.repository.rootPath, pkg.location);
-    const diff = GitUtilities.diffSinceIn(commits, pkg.location, this.execOpts);
+function makeDiffSince(rootPath, execOpts, ignorePatterns) {
+  return function hasDiffSinceThatIsntIgnored(pkg, commits) {
+    const folder = path.relative(rootPath, pkg.location);
+    const diff = GitUtilities.diffSinceIn(commits, pkg.location, execOpts);
 
     if (diff === "") {
       return false;
@@ -206,13 +45,123 @@ class UpdatedPackagesCollector {
 
     let changedFiles = diff.split("\n").map(file => file.replace(folder + path.sep, ""));
 
-    if (this.options.ignore) {
+    if (ignorePatterns) {
       changedFiles = changedFiles.filter(
-        file => !_.find(this.options.ignore, pattern => minimatch(file, pattern, { matchBase: true }))
+        file => !_.find(ignorePatterns, pattern => minimatch(file, pattern, { matchBase: true }))
       );
     }
 
     return !!changedFiles.length;
+  };
+}
+
+class UpdatedPackagesCollector {
+  constructor(command) {
+    this.execOpts = command.execOpts;
+    this.logger = command.logger;
+    this.rootPath = command.repository.rootPath;
+    this.options = command.options;
+
+    if (command.filteredPackages.length === command.packageGraph.size) {
+      this.packages = command.packageGraph;
+    } else {
+      this.packages = new Map(
+        command.filteredPackages.map(({ name }) => [name, command.packageGraph.get(name)])
+      );
+    }
+  }
+
+  getUpdates() {
+    this.logger.silly("getUpdates");
+
+    const updatedPackages = this.collectUpdatedPackages();
+    const prereleasedPackages = this.collectPrereleasedPackages();
+    const dependents = new Set(
+      [...updatedPackages, ...prereleasedPackages].reduce(
+        (arr, node) => arr.concat(...node.localDependents),
+        []
+      )
+    );
+
+    const updates = [];
+
+    const mapper = (node, name) => {
+      this.logger.verbose("has filtered update", name);
+
+      // TODO: stop re-wrapping with a silly Update class
+      updates.push(new Update(node.pkg));
+    };
+
+    if (this.options.canary) {
+      this.packages.forEach(mapper);
+    } else {
+      this.packages.forEach((node, name) => {
+        if (updatedPackages.has(node) || prereleasedPackages.has(node) || dependents.has(node)) {
+          mapper(node, name);
+        }
+      });
+    }
+
+    return updates;
+  }
+
+  collectUpdatedPackages() {
+    this.logger.info("", "Checking for updated packages...");
+
+    const { execOpts, options, rootPath } = this;
+    const { canary, forcePublish, ignore: ignorePatterns } = options;
+    let { since } = options;
+
+    if (GitUtilities.hasTags(execOpts)) {
+      if (canary) {
+        const sha = GitUtilities.getShortSHA(execOpts);
+
+        // if it's a merge commit, it will return all the commits that were part of the merge
+        // ex: If `ab7533e` had 2 commits, ab7533e^..ab7533e would contain 2 commits + the merge commit
+        since = `${sha}^..${sha}`;
+      } else if (!since) {
+        since = GitUtilities.getLastTag(execOpts);
+      }
+    }
+
+    this.logger.info("", `Comparing with ${since || "initial commit"}.`);
+
+    const updatedPackages = new Set();
+    const forced = getForcedPackages(forcePublish);
+
+    if (!since || forced.has("*")) {
+      this.packages.forEach(node => updatedPackages.add(node));
+    } else {
+      const hasDiffSinceThatIsntIgnored = makeDiffSince(rootPath, execOpts, ignorePatterns);
+
+      this.packages.forEach((node, name) => {
+        if (forced.has(name) || hasDiffSinceThatIsntIgnored(node, since)) {
+          updatedPackages.add(node);
+        }
+      });
+    }
+
+    return updatedPackages;
+  }
+
+  collectPrereleasedPackages() {
+    this.logger.info("", "Checking for prereleased packages...");
+
+    const prereleasedPackages = new Set();
+
+    if ((this.options.cdVersion || "").startsWith("pre")) {
+      return prereleasedPackages;
+    }
+
+    // skip packages that have not been previously prereleased
+    this.packages.forEach((node, name) => {
+      if (semver.prerelease(node.version)) {
+        this.logger.verbose("prereleased", name);
+        prereleasedPackages.add(node);
+      }
+    });
+
+    return prereleasedPackages;
   }
 }
 

src/commands/PublishCommand.js
@@ -225,10 +225,10 @@ class PublishCommand extends Command {
     this.packagesToPublishCount = this.packagesToPublish.length;
     try {
       this.batchedPackagesToPublish = this.toposort
-        ? PackageUtilities.topologicallyBatchPackages(this.packagesToPublish, {
+        ? PackageUtilities.batchPackages(this.packagesToPublish, {
             // Don't sort based on devDependencies because that would increase the chance of dependency cycles
             // causing less-than-ideal a publishing order.
-            depsOnly: true,
+            graphType: "dependencies",
             rejectCycles: this.options.rejectCycles,
           })
         : [this.packagesToPublish];
@@ -524,8 +524,7 @@ class PublishCommand extends Command {
   }
 
   confirmVersions(callback) {
-    const changes = this.updates.map(update => {
-      const pkg = update.package;
+    const changes = this.updates.map(({ package: pkg }) => {
       let line = ` - ${pkg.name}: ${pkg.version} => ${this.updatesVersions[pkg.name]}`;
       if (pkg.private) {
         line += ` (${chalk.red("private")})`;
@@ -637,7 +636,7 @@ class PublishCommand extends Command {
       return;
     }
 
-    this.packageGraph.get(pkg.name).dependencies.forEach(depName => {
+    this.packageGraph.get(pkg.name).localDependencies.forEach((depNode, depName) => {
       const version = this.updatesVersions[depName];
 
       if (deps[depName] && version) {
@@ -654,8 +653,8 @@ class PublishCommand extends Command {
     }
 
     // run the postversion script for each update
-    this.updates.forEach(update => {
-      this.runSyncScriptInPackage(update.package, "postversion");
+    this.updates.forEach(({ package: pkg }) => {
+      this.runSyncScriptInPackage(pkg, "postversion");
     });
 
     // run postversion, if set in the root directory
@@ -703,9 +702,7 @@ class PublishCommand extends Command {
     // therefore no updates will be needed
     const tag = this.options.tempTag ? "lerna-temp" : this.getDistTag();
 
-    this.updates.forEach(update => {
-      this.execScript(update.package, "prepublish");
-    });
+    this.updates.forEach(({ package: pkg }) => this.execScript(pkg, "prepublish"));
 
     tracker.addWork(this.packagesToPublishCount);
 

test/UpdatedPackagesCollector.js
@@ -19,6 +19,8 @@ const filteredPackages = [
   },
 ];
 
+const packageGraph = new Map(filteredPackages.map(pkg => [pkg.name, pkg]));
+
 const logger = {
   silly: () => {},
   info: () => {},
@@ -36,7 +38,7 @@ describe("UpdatedPackagesCollector", () => {
 
   describe(".collectUpdatedPackages()", () => {
     beforeEach(() => {
-      GitUtilities.getCurrentSHA = jest.fn(() => "deadbeefcafe");
+      GitUtilities.getShortSHA = jest.fn(() => "deadbeef");
       GitUtilities.hasTags = jest.fn(() => true);
       GitUtilities.diffSinceIn = jest.fn(() => "");
       GitUtilities.getLastTag = jest.fn(() => "lastTag");
@@ -52,6 +54,7 @@ describe("UpdatedPackagesCollector", () => {
         repository,
         logger,
         filteredPackages,
+        packageGraph,
       }).getUpdates();
 
       expect(GitUtilities.diffSinceIn).toBeCalledWith("deadbeef^..deadbeef", "location-1", undefined);
@@ -66,6 +69,7 @@ describe("UpdatedPackagesCollector", () => {
         repository,
         logger,
         filteredPackages,
+        packageGraph,
       }).getUpdates();
 
       expect(GitUtilities.diffSinceIn).toBeCalledWith("deadbeef^..deadbeef", "location-1", undefined);
@@ -78,6 +82,7 @@ describe("UpdatedPackagesCollector", () => {
         repository,
         logger,
         filteredPackages,
+        packageGraph,
       }).getUpdates();
 
       expect(GitUtilities.diffSinceIn).toBeCalledWith("lastTag", "location-1", undefined);

src/Command.js
@@ -275,14 +275,15 @@ class Command {
     }
 
     try {
-      const versionParser = useGitVersion && new GitVersionParser(gitVersionPrefix);
       const packages = PackageUtilities.getPackages({ rootPath, packageConfigs });
-      const packageGraph = PackageUtilities.getPackageGraph(packages, false, versionParser);
+      const packageGraph = PackageUtilities.getPackageGraph(packages);
 
       if (useGitVersion) {
+        const versionParser = new GitVersionParser(gitVersionPrefix);
+
         packages.forEach(pkg => {
           pkg.versionSerializer = new VersionSerializer({
-            graphDependencies: packageGraph.get(pkg.name).dependencies,
+            localDependencies: packageGraph.get(pkg.name).localDependencies,
             versionParser,
           });
         });

commit 56cce9c6e803296bfbd48f69a7e9a6c06153059e

chore: Fix transitive dependent collection broken in #1260

src/UpdatedPackagesCollector.js
@@ -74,14 +74,11 @@ class UpdatedPackagesCollector {
   getUpdates() {
     this.logger.silly("getUpdates");
 
-    const updatedPackages = this.collectUpdatedPackages();
-    const prereleasedPackages = this.collectPrereleasedPackages();
-    const dependents = new Set(
-      [...updatedPackages, ...prereleasedPackages].reduce(
-        (arr, node) => arr.concat(...node.localDependents),
-        []
-      )
-    );
+    this.candidates = new Set();
+
+    this.collectUpdatedPackages();
+    this.collectPrereleasedPackages();
+    this.collectTransitiveDependents();
 
     const updates = [];
 
@@ -96,7 +93,7 @@ class UpdatedPackagesCollector {
       this.packages.forEach(mapper);
     } else {
       this.packages.forEach((node, name) => {
-        if (updatedPackages.has(node) || prereleasedPackages.has(node) || dependents.has(node)) {
+        if (this.candidates.has(node)) {
           mapper(node, name);
         }
       });
@@ -126,42 +123,70 @@ class UpdatedPackagesCollector {
 
     this.logger.info("", `Comparing with ${since || "initial commit"}.`);
 
-    const updatedPackages = new Set();
     const forced = getForcedPackages(forcePublish);
 
     if (!since || forced.has("*")) {
-      this.packages.forEach(node => updatedPackages.add(node));
+      this.packages.forEach(node => this.candidates.add(node));
     } else {
       const hasDiffSinceThatIsntIgnored = makeDiffSince(rootPath, execOpts, ignorePatterns);
 
       this.packages.forEach((node, name) => {
         if (forced.has(name) || hasDiffSinceThatIsntIgnored(node, since)) {
-          updatedPackages.add(node);
+          this.candidates.add(node);
         }
       });
     }
-
-    return updatedPackages;
   }
 
   collectPrereleasedPackages() {
-    this.logger.info("", "Checking for prereleased packages...");
-
-    const prereleasedPackages = new Set();
-
     if ((this.options.cdVersion || "").startsWith("pre")) {
-      return prereleasedPackages;
+      return;
     }
+
+    this.logger.info("", "Checking for prereleased packages...");
 
     // skip packages that have not been previously prereleased
     this.packages.forEach((node, name) => {
       if (semver.prerelease(node.version)) {
         this.logger.verbose("prereleased", name);
-        prereleasedPackages.add(node);
+        this.candidates.add(node);
       }
     });
+  }
 
-    return prereleasedPackages;
+  collectTransitiveDependents() {
+    const collected = new Set();
+
+    this.candidates.forEach((currentNode, currentName) => {
+      if (currentNode.localDependents.size === 0) {
+        // no point diving into a non-existent tree
+        return;
+      }
+
+      // depth-first search, whee
+      const seen = new Set();
+
+      const visit = (dependentNode, dependentName, siblingDependents) => {
+        if (seen.has(dependentNode)) {
+          return;
+        }
+
+        seen.add(dependentNode);
+
+        if (dependentNode === currentNode || siblingDependents.has(currentName)) {
+          // a direct or transitive cycle, skip it
+          return;
+        }
+
+        collected.add(dependentNode);
+
+        dependentNode.localDependents.forEach(visit);
+      };
+
+      currentNode.localDependents.forEach(visit);
+    });
+
+    collected.forEach(node => this.candidates.add(node));
   }
 }
 

test/PublishCommand.js
@@ -23,8 +23,8 @@ const commandModule = require("../src/commands/PublishCommand");
 
 const run = yargsRunner(commandModule);
 
-jest.mock("write-json-file", () => jest.fn(() => Promise.resolve()));
-jest.mock("write-pkg", () => jest.fn(() => Promise.resolve()));
+jest.mock("write-json-file");
+jest.mock("write-pkg");
 jest.mock("../src/GitUtilities");
 jest.mock("../src/NpmUtilities");
 jest.mock("../src/PromptUtilities");
@@ -32,11 +32,6 @@ jest.mock("../src/ConventionalCommitUtilities");
 
 // silence logs
 log.level = "silent";
-
-const execOpts = testDir =>
-  expect.objectContaining({
-    cwd: testDir,
-  });
 
 const publishedTagInDirectories = testDir =>
   NpmUtilities.publishTaggedInDir.mock.calls.reduce((arr, args) => {
@@ -151,7 +146,13 @@ describe("PublishCommand", () => {
 
       expect(publishedTagInDirectories(testDir)).toMatchSnapshot("npm published");
 
-      expect(GitUtilities.pushWithTags).lastCalledWith("origin", gitTagsAdded(), execOpts(testDir));
+      expect(GitUtilities.pushWithTags).lastCalledWith(
+        "origin",
+        gitTagsAdded(),
+        expect.objectContaining({
+          cwd: testDir,
+        })
+      );
       expect(consoleOutput()).toMatchSnapshot("console output");
     });
 
@@ -174,7 +175,7 @@ describe("PublishCommand", () => {
     it("should publish the changed packages in independent mode", async () => {
       // mock version prompt choices
       ["1.0.1", "1.1.0", "2.0.0", "1.1.0", "1.0.1"].forEach(chosenVersion =>
-        PromptUtilities.select.mockReturnValueOnce(Promise.resolve(chosenVersion))
+        PromptUtilities.select.mockResolvedValueOnce(chosenVersion)
       );
 
       const testDir = await initFixture("PublishCommand/independent");
@@ -205,7 +206,13 @@ describe("PublishCommand", () => {
 
       expect(publishedTagInDirectories(testDir)).toMatchSnapshot("npm published");
 
-      expect(GitUtilities.pushWithTags).lastCalledWith("origin", gitTagsAdded(), execOpts(testDir));
+      expect(GitUtilities.pushWithTags).lastCalledWith(
+        "origin",
+        gitTagsAdded(),
+        expect.objectContaining({
+          cwd: testDir,
+        })
+      );
       expect(consoleOutput()).toMatchSnapshot("console output");
     });
   });
@@ -239,7 +246,9 @@ describe("PublishCommand", () => {
       expect(GitUtilities.addTag).not.toBeCalled();
       expect(GitUtilities.checkoutChanges).lastCalledWith(
         expect.stringContaining("packages/*/package.json"),
-        execOpts(testDir)
+        expect.objectContaining({
+          cwd: testDir,
+        })
       );
 
       expect(GitUtilities.pushWithTags).not.toBeCalled();
@@ -412,7 +421,13 @@ describe("PublishCommand", () => {
       expect(removedDistTagInDirectories(testDir)).toMatchSnapshot("npm dist-tag rm");
       expect(addedDistTagInDirectories(testDir)).toMatchSnapshot("npm dist-tag add");
 
-      expect(GitUtilities.pushWithTags).lastCalledWith("origin", ["v1.0.1"], execOpts(testDir));
+      expect(GitUtilities.pushWithTags).lastCalledWith(
+        "origin",
+        ["v1.0.1"],
+        expect.objectContaining({
+          cwd: testDir,
+        })
+      );
     });
   });
 
@@ -631,7 +646,13 @@ describe("PublishCommand", () => {
       const testDir = await initFixture("PublishCommand/normal");
       await run(testDir)("--git-remote", "upstream");
 
-      expect(GitUtilities.pushWithTags).lastCalledWith("upstream", ["v1.0.1"], execOpts(testDir));
+      expect(GitUtilities.pushWithTags).lastCalledWith(
+        "upstream",
+        ["v1.0.1"],
+        expect.objectContaining({
+          cwd: testDir,
+        })
+      );
     });
   });
 
@@ -658,14 +679,24 @@ describe("PublishCommand", () => {
       const testDir = await initFixture("PublishCommand/normal");
       await run(testDir)("--message", "chore: Release %s :rocket:");
 
-      expect(GitUtilities.commit).lastCalledWith("chore: Release v1.0.1 :rocket:", execOpts(testDir));
+      expect(GitUtilities.commit).lastCalledWith(
+        "chore: Release v1.0.1 :rocket:",
+        expect.objectContaining({
+          cwd: testDir,
+        })
+      );
     });
 
     it("commits changes with a custom message using %v", async () => {
       const testDir = await initFixture("PublishCommand/normal");
       await run(testDir)("--message", "chore: Release %v :rocket:");
 
-      expect(GitUtilities.commit).lastCalledWith("chore: Release 1.0.1 :rocket:", execOpts(testDir));
+      expect(GitUtilities.commit).lastCalledWith(
+        "chore: Release 1.0.1 :rocket:",
+        expect.objectContaining({
+          cwd: testDir,
+        })
+      );
     });
   });
 
@@ -678,7 +709,12 @@ describe("PublishCommand", () => {
       const testDir = await initFixture("PublishCommand/independent");
       await run(testDir)("-m", "chore: Custom publish message");
 
-      expect(GitUtilities.commit).lastCalledWith(expect.stringContaining("chore:"), execOpts(testDir));
+      expect(GitUtilities.commit).lastCalledWith(
+        expect.stringContaining("chore: Custom publish message"),
+        expect.objectContaining({
+          cwd: testDir,
+        })
+      );
       expect(gitCommitMessage()).toMatchSnapshot("git commit message");
     });
   });
@@ -688,18 +724,16 @@ describe("PublishCommand", () => {
    * ======================================================================= */
 
   describe("--conventional-commits", () => {
-    beforeEach(() => {
-      ConventionalCommitUtilities.updateChangelog.mockImplementation(pkg =>
-        Promise.resolve(path.join(pkg.location, "CHANGELOG.md"))
-      );
-    });
+    ConventionalCommitUtilities.updateChangelog.mockImplementation(pkg =>
+      Promise.resolve(path.join(pkg.location, "CHANGELOG.md"))
+    );
 
     describe("independent mode", () => {
       const versionBumps = ["1.0.1", "2.1.0", "4.0.0", "4.1.0", "5.0.1"];
 
       beforeEach(() => {
         versionBumps.forEach(bump =>
-          ConventionalCommitUtilities.recommendVersion.mockReturnValueOnce(Promise.resolve(bump))
+          ConventionalCommitUtilities.recommendVersion.mockResolvedValueOnce(bump)
         );
       });
 
@@ -747,11 +781,11 @@ describe("PublishCommand", () => {
     describe("fixed mode", () => {
       beforeEach(() => {
         ConventionalCommitUtilities.recommendVersion
-          .mockReturnValueOnce(Promise.resolve("1.0.1"))
-          .mockReturnValueOnce(Promise.resolve("1.1.0"))
-          .mockReturnValueOnce(Promise.resolve("2.0.0"))
-          .mockReturnValueOnce(Promise.resolve("1.1.0"))
-          .mockReturnValueOnce(Promise.resolve("1.0.0"));
+          .mockResolvedValueOnce("1.0.1")
+          .mockResolvedValueOnce("1.1.0")
+          .mockResolvedValueOnce("2.0.0")
+          .mockResolvedValueOnce("1.1.0")
+          .mockResolvedValueOnce("1.0.0");
       });
 
       it("should use conventional-commits utility to guess version bump and generate CHANGELOG", async () => {
@@ -781,7 +815,7 @@ describe("PublishCommand", () => {
         expect(ConventionalCommitUtilities.updateChangelog).lastCalledWith(
           expect.objectContaining({
             name: "normal",
-            location: path.join(testDir),
+            location: testDir,
           }),
           "root",
           { changelogPreset: undefined, version: "2.0.0" }
@@ -947,5 +981,17 @@ describe("PublishCommand", () => {
 
       expect(NpmUtilities.runScriptInDirSync.mock.calls.map(args => args[0])).toEqual(scripts);
     });
+  });
+
+  it("publishes all transitive dependents after change", async () => {
+    const testDir = await initFixture("PublishCommand/snake-graph");
+
+    GitUtilities.hasTags.mockReturnValueOnce(true);
+    GitUtilities.getLastTag.mockReturnValueOnce("v1.0.0");
+    GitUtilities.diffSinceIn.mockReturnValueOnce("packages/package-1/package.json");
+
+    await run(testDir)("--cd-version", "major", "--yes");
+
+    expect(updatedPackageVersions(testDir)).toMatchSnapshot();
   });
 });

test/__snapshots__/PublishCommand.js.snap
@@ -518,3 +518,13 @@ Object {
   "packages/package-3": "1.0.1-beta.4",
 }
 `;
+
+exports[`PublishCommand publishes all transitive dependents after change 1`] = `
+Object {
+  "packages/package-1": "2.0.0",
+  "packages/package-2": "2.0.0",
+  "packages/package-3": "2.0.0",
+  "packages/package-4": "2.0.0",
+  "packages/package-5": "2.0.0",
+}
+`;

test/integration/__snapshots__/lerna-publish.test.js.snap
@@ -243,6 +243,49 @@ See [Conventional Commits](https://conventionalcommits.org) for commit guideline
 ]
 `;
 
+exports[`lerna publish updates all transitive dependents 1`] = `
+Array [
+  Object {
+    change: true,
+    description: no local dependencies, four local dependents (three transitive),
+    name: package-1,
+    version: 2.0.0,
+  },
+  Object {
+    dependencies: Object {
+      package-1: ^2.0.0,
+    },
+    description: one local dependency, one direct dependent, no transitive dependencies,
+    name: package-2,
+    version: 2.0.0,
+  },
+  Object {
+    dependencies: Object {
+      package-2: ^2.0.0,
+    },
+    description: one local dependency, one direct dependent, one transitive dependency,
+    name: package-3,
+    version: 2.0.0,
+  },
+  Object {
+    dependencies: Object {
+      package-3: ^2.0.0,
+    },
+    description: one local dependency, one direct dependent, two transitive dependencies,
+    name: package-4,
+    version: 2.0.0,
+  },
+  Object {
+    dependencies: Object {
+      package-4: ^2.0.0,
+    },
+    description: one local dependency, no dependents, three transitive dependencies,
+    name: package-5,
+    version: 2.0.0,
+  },
+]
+`;
+
 exports[`lerna publish updates fixed versions: commit 1`] = `
 v1.0.1
 

test/integration/lerna-publish.test.js
@@ -91,6 +91,18 @@ describe("lerna publish", () => {
     expect(commitMessage).toMatchSnapshot("commit");
   });
 
+  test("updates all transitive dependents", async () => {
+    const cwd = await initFixture("PublishCommand/snake-graph");
+    const args = ["publish", "--skip-npm", "--cd-version=major", "--yes"];
+
+    await execa("git", ["tag", "v1.0.0", "-m", "v1.0.0"], { cwd });
+    await commitChangeToPackage(cwd, "package-1", "change", { change: true });
+
+    await runner(cwd)(...args);
+
+    expect(await loadPkgManifests(cwd)).toMatchSnapshot();
+  });
+
   test("uses default suffix with canary flag", async () => {
     const cwd = await initFixture("PublishCommand/normal");
     const args = ["publish", "--canary", "--skip-npm", "--yes"];