616cd0a53addefcd4f41a3a8179958aa5785ccb6
Daniel Stockman <daniel.stockman@gmail.com>
2018-02-16 13:01:40 -0800

    refactor: use simpler predicates in snapshot serializer

--- test/helpers/serializePlaceholders.js
+++ test/helpers/serializePlaceholders.js
@@ -3,13 +3,20 @@
 
 "use strict";
 
-const _ = require("lodash");
 const normalizeNewline = require("normalize-newline");
 const constants = require("./constants");
 
 const VERSION_REGEX = new RegExp(`^(?:((?:.*?version )|\\^)|v?)${constants.LERNA_VERSION}`, "gm");
 const VERSION_REPLACEMENT = `$1${constants.__TEST_VERSION__}`;
 // TODO: maybe even less naïve regex?
+
+function isObject(val) {
+  return val && typeof val === "object" && Array.isArray(val) === false;
+}
+
+function isString(val) {
+  return val && typeof val === "string";
+}
 
 function needsReplacement(str) {
   return str.indexOf(constants.__TEST_VERSION__) === -1;
@@ -19,7 +26,9 @@ function stableVersion(str) {
   return str.replace(VERSION_REGEX, VERSION_REPLACEMENT);
 }
 
-const stabilizeString = _.flow([normalizeNewline, stableVersion]);
+function stabilizeString(str) {
+  return stableVersion(normalizeNewline(str));
+}
 
 /**
  * A snapshot serializer that replaces all instances of unstable version number
@@ -29,19 +38,15 @@ const stabilizeString = _.flow([normalizeNewline, stableVersion]);
  */
 module.exports = {
   test(thing) {
-    if (_.isString(thing) && needsReplacement(thing)) {
-      return true;
+    if (isObject(thing) && isString(thing.lerna)) {
+      return needsReplacement(thing.lerna);
     }
 
-    if (_.isPlainObject(thing) && _.isString(thing.lerna) && needsReplacement(thing.lerna)) {
-      return true;
-    }
-
-    return false;
+    return isString(thing) && needsReplacement(thing);
   },
 
   print(thing, serialize) {
-    if (_.isString(thing)) {
+    if (isString(thing)) {
       return stabilizeString(thing);
     }
 
