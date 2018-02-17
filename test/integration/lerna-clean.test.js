3945eda906718a7e9fcd1b514c6b95455b8a32fa
Daniel Stockman <daniel.stockman@gmail.com>
2018-02-16 17:15:08 -0800

    chore: shuffle around integration test assertion helpers

--- test/integration/lerna-clean.test.js
+++ test/integration/lerna-clean.test.js
@@ -8,22 +8,22 @@ const path = require("path");
 const { LERNA_BIN } = require("../helpers/constants");
 const initFixture = require("../helpers/initFixture");
 
-const serializeTestRoot = (match, testDir, subPath) => normalizePath(path.join("__TEST_ROOTDIR__", subPath));
-
-const normalizeLog = cwd => {
-  // lol windows paths often look like escaped slashes, so re-re-escape them :P
-  const dirPath = new RegExp(`(${cwd.replace(/\\/g, "\\\\")})([\\S]+)`, "g");
-
-  return stderr => stderr.replace(dirPath, serializeTestRoot);
-};
-
 describe("lerna clean", () => {
+  const serializeTestRoot = (match, testDir, subPath) =>
+    normalizePath(path.join("__TEST_ROOTDIR__", subPath));
+
+  const normalizeLog = cwd => {
+    // lol windows paths often look like escaped slashes, so re-re-escape them :P
+    const dirPath = new RegExp(`(${cwd.replace(/\\/g, "\\\\")})([\\S]+)`, "g");
+
+    return stderr => stderr.replace(dirPath, serializeTestRoot);
+  };
+
   test("global", async () => {
     const cwd = await initFixture("CleanCommand/basic");
     const args = ["clean", "--yes", "--concurrency=1"];
 
-    const { stderr } = await execa(LERNA_BIN, args, { cwd });
-
+    const stderr = await execa.stderr(LERNA_BIN, args, { cwd });
     expect(normalizeLog(cwd)(stderr)).toMatchSnapshot("stderr");
 
     const found = await globby(["package-*/node_modules"], { cwd });
--- test/integration/lerna-init.test.js
+++ test/integration/lerna-init.test.js
@@ -8,17 +8,15 @@ const tempy = require("tempy");
 const { LERNA_BIN } = require("../helpers/constants");
 const initFixture = require("../helpers/initFixture");
 
-const parsePackageJson = cwd => loadJsonFile(path.join(cwd, "package.json"));
-
-const parseLernaJson = cwd => loadJsonFile(path.join(cwd, "lerna.json"));
-
-const loadMetaData = cwd => Promise.all([parsePackageJson(cwd), parseLernaJson(cwd)]);
-
 describe("lerna init", () => {
+  const parsePackageJson = cwd => loadJsonFile(path.join(cwd, "package.json"));
+  const parseLernaJson = cwd => loadJsonFile(path.join(cwd, "lerna.json"));
+  const loadMetaData = cwd => Promise.all([parsePackageJson(cwd), parseLernaJson(cwd)]);
+
   test("initializes empty directory", async () => {
     const cwd = tempy.directory();
 
-    const { stderr } = await execa(LERNA_BIN, ["init"], { cwd });
+    const stderr = await execa.stderr(LERNA_BIN, ["init"], { cwd });
     expect(stderr).toMatchSnapshot("stderr");
 
     const [packageJson, lernaJson] = await loadMetaData(cwd);
@@ -29,7 +27,7 @@ describe("lerna init", () => {
   test("updates existing metadata", async () => {
     const cwd = await initFixture("InitCommand/updates");
 
-    const { stderr } = await execa(LERNA_BIN, ["init", "--exact"], { cwd });
+    const stderr = await execa.stderr(LERNA_BIN, ["init", "--exact"], { cwd });
     expect(stderr).toMatchSnapshot("stderr");
 
     const [packageJson, lernaJson] = await loadMetaData(cwd);
