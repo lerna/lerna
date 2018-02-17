d0d378c2957e0d9497b175a96ab055b33362d873
Daniel Stockman <daniel.stockman@gmail.com>
2018-02-16 16:48:05 -0800

    chore: reduce nesting of lerna-bootstrap test cases

--- test/integration/lerna-bootstrap.test.js
+++ test/integration/lerna-bootstrap.test.js
@@ -24,52 +24,50 @@ describe("lerna bootstrap", () => {
       { cwd }
     );
 
-  describe("from CLI", () => {
-    test("bootstraps all packages", async () => {
-      const cwd = await initFixture("BootstrapCommand/integration");
-      const args = ["bootstrap"];
+  test("bootstraps all packages", async () => {
+    const cwd = await initFixture("BootstrapCommand/integration");
+    const args = ["bootstrap"];
 
-      const stderr = await execa.stderr(LERNA_BIN, args, { cwd });
-      expect(stderr).toMatchSnapshot("stderr");
+    const stderr = await execa.stderr(LERNA_BIN, args, { cwd });
+    expect(stderr).toMatchSnapshot("stderr");
 
-      const { stdout } = await npmTest(cwd);
-      expect(stdout).toMatchSnapshot("stdout");
-    });
+    const { stdout } = await npmTest(cwd);
+    expect(stdout).toMatchSnapshot("stdout");
+  });
 
-    test("--npm-client yarn", async () => {
-      const cwd = await initFixture("BootstrapCommand/integration");
-      const args = ["bootstrap", "--npm-client", "yarn"];
+  test("--npm-client yarn", async () => {
+    const cwd = await initFixture("BootstrapCommand/integration");
+    const args = ["bootstrap", "--npm-client", "yarn"];
 
-      const stderr = await execa.stderr(LERNA_BIN, args, { cwd });
-      expect(stderr).toMatchSnapshot("stderr");
+    const stderr = await execa.stderr(LERNA_BIN, args, { cwd });
+    expect(stderr).toMatchSnapshot("stderr");
 
-      const lockfiles = await globby(["package-*/yarn.lock"], { cwd }).then(globbed =>
-        globbed.map(fp => normalizePath(fp))
-      );
-      expect(lockfiles).toMatchSnapshot("lockfiles");
+    const lockfiles = await globby(["package-*/yarn.lock"], { cwd }).then(globbed =>
+      globbed.map(fp => normalizePath(fp))
+    );
+    expect(lockfiles).toMatchSnapshot("lockfiles");
 
-      const { stdout } = await npmTest(cwd);
-      expect(stdout).toMatchSnapshot("stdout");
-    });
+    const { stdout } = await npmTest(cwd);
+    expect(stdout).toMatchSnapshot("stdout");
+  });
 
-    test("passes remaining arguments to npm client", async () => {
-      const cwd = await initFixture("BootstrapCommand/npm-client-args-1");
-      const args = ["bootstrap", "--npm-client", path.resolve(cwd, "npm"), "--", "--no-optional"];
+  test("passes remaining arguments to npm client", async () => {
+    const cwd = await initFixture("BootstrapCommand/npm-client-args-1");
+    const args = ["bootstrap", "--npm-client", path.resolve(cwd, "npm"), "--", "--no-optional"];
 
-      await execa(LERNA_BIN, args, { cwd });
+    await execa(LERNA_BIN, args, { cwd });
 
-      const npmDebugLog = fs.readFileSync(path.resolve(cwd, "npm-debug.log")).toString();
-      expect(npmDebugLog).toMatchSnapshot();
-    });
+    const npmDebugLog = fs.readFileSync(path.resolve(cwd, "npm-debug.log")).toString();
+    expect(npmDebugLog).toMatchSnapshot();
+  });
 
-    test("passes remaining arguments + npmClientArgs to npm client", async () => {
-      const cwd = await initFixture("BootstrapCommand/npm-client-args-2");
-      const args = ["bootstrap", "--npm-client", path.resolve(cwd, "npm"), "--", "--no-optional"];
+  test("passes remaining arguments + npmClientArgs to npm client", async () => {
+    const cwd = await initFixture("BootstrapCommand/npm-client-args-2");
+    const args = ["bootstrap", "--npm-client", path.resolve(cwd, "npm"), "--", "--no-optional"];
 
-      await execa(LERNA_BIN, args, { cwd });
+    await execa(LERNA_BIN, args, { cwd });
 
-      const npmDebugLog = fs.readFileSync(path.resolve(cwd, "npm-debug.log")).toString();
-      expect(npmDebugLog).toMatchSnapshot();
-    });
+    const npmDebugLog = fs.readFileSync(path.resolve(cwd, "npm-debug.log")).toString();
+    expect(npmDebugLog).toMatchSnapshot();
   });
 });
