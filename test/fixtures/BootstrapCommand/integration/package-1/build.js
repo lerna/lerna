3e7f76bd5997a96d1e4fd141dc0071ee9590b1d6
Daniel Stockman <daniel.stockman@gmail.com>
2018-02-27 12:07:40 -0800

    use node 6.x-compatible fs APIs in integration build script

--- test/fixtures/BootstrapCommand/integration/package-1/build.js
+++ test/fixtures/BootstrapCommand/integration/package-1/build.js
@@ -6,4 +6,8 @@ const path = require("path");
 const indexSource = path.resolve("./index.src.js");
 const indexTarget = path.resolve("./index.js");
 
-fs.copyFileSync(indexSource, indexTarget);
+const reader = fs.createReadStream(indexSource);
+const writer = fs.createWriteStream(indexTarget);
+
+// fs.copyFileSync is node >=8.5.0
+reader.pipe(writer);
