430d000ff9a03425e5684b7d259a93cbefa4d369
Daniel Stockman <daniel.stockman@gmail.com>
2018-03-04 14:48:10 -0800

    refactor(init-fixture): partially-applied findUp __fixtures__

--- helpers/init-fixture/copy-fixture.js
+++ helpers/init-fixture/copy-fixture.js
@@ -1,12 +1,13 @@
 "use strict";
 
+const findUp = require("find-up");
 const fs = require("fs-extra");
 const path = require("path");
 
 module.exports = copyFixture;
 
-function copyFixture(targetDir, fixturePath) {
-  const fixtureDir = path.resolve(__dirname, `../fixtures/${fixturePath}`);
-
-  return fs.copy(fixtureDir, targetDir);
+function copyFixture(targetDir, fixtureName, cwd) {
+  return Promise.resolve()
+    .then(() => findUp(path.join("__fixtures__", fixtureName), { cwd }))
+    .then(fixtureDir => fs.copy(fixtureDir, targetDir));
 }
--- helpers/init-fixture/index.js
+++ helpers/init-fixture/index.js
@@ -6,11 +6,13 @@ const gitInit = require("./git-init");
 
 module.exports = initFixture;
 
-function initFixture(fixturePath, commitMessage = "Init commit") {
-  const testDir = tempy.directory();
+function initFixture(startDir) {
+  return (fixtureName, commitMessage = "Init commit") => {
+    const cwd = tempy.directory();
 
-  return Promise.resolve()
-    .then(() => copyFixture(testDir, fixturePath))
-    .then(() => gitInit(testDir, commitMessage))
-    .then(() => testDir);
+    return Promise.resolve()
+      .then(() => copyFixture(cwd, fixtureName, startDir))
+      .then(() => gitInit(cwd, commitMessage))
+      .then(() => cwd);
+  };
 }
