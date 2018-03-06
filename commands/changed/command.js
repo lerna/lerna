924c115f2723a66d5ef5f3cf7e3f221b38adc1b7
Daniel Stockman <daniel.stockman@gmail.com>
2018-03-06 09:10:02 -0800

    refactor: Share options between publish and changed as a builder function

--- commands/changed/command.js
+++ commands/changed/command.js
@@ -10,16 +10,14 @@ exports.aliases = ["updated"];
 exports.describe = "Check which packages have changed since the last publish.";
 
 exports.builder = yargs =>
-  yargs.options(
-    Object.assign({}, publishOptions, {
-      json: {
-        describe: "Show information in JSON format",
-        group: "Command Options:",
-        type: "boolean",
-        default: undefined,
-      },
-    })
-  );
+  publishOptions(yargs).options({
+    json: {
+      describe: "Show information in JSON format",
+      group: "Command Options:",
+      type: "boolean",
+      default: undefined,
+    },
+  });
 
 exports.handler = function handler(argv) {
   return new ChangedCommand(argv);
