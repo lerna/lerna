591756304b94974985de4d7f90256c18bbf87fa8
Daniel Stockman <daniel.stockman@gmail.com>
2018-03-05 17:58:25 -0800

    move jest config out of package.json, update for monorepo

--- jest.integration.js
+++ jest.integration.js
@@ -1,16 +1,11 @@
-"use strict"; // eslint-disable-line strict, lines-around-directive
-
-const path = require("path");
-
-const integrationRootDir = path.resolve(__dirname, "./test/integration");
-const setupTestFramework = path.join(integrationRootDir, "_setupTestFramework.js");
-const serializePlaceholders = path.resolve(__dirname, "./test/helpers/serializePlaceholders.js");
+"use strict";
 
 module.exports = {
   bail: true,
-  roots: [integrationRootDir],
+  modulePathIgnorePatterns: ["<rootDir>/.*/__fixtures__"],
+  roots: ["<rootDir>/integration"],
+  setupTestFrameworkScriptFile: "<rootDir>/setup-integration-timeout.js",
+  snapshotSerializers: ["@lerna-test/serialize-placeholders"],
   testEnvironment: "node",
-  setupTestFrameworkScriptFile: setupTestFramework,
-  snapshotSerializers: [serializePlaceholders],
   verbose: true,
 };
