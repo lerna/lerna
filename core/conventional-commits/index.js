64916d650233f939d49d34ec52c691037f89106c
Daniel Stockman <daniel.stockman@gmail.com>
2018-03-07 17:02:54 -0800

    fix(conventional-commits): output version bump message closer to version heading

--- core/conventional-commits/index.js
+++ core/conventional-commits/index.js
@@ -101,13 +101,13 @@ function recommendVersion(pkg, type, { changelogPreset }) {
 function makeBumpOnlyFilter(pkg) {
   return newEntry => {
     // When force publishing, it is possible that there will be no actual changes, only a version bump.
-    // Add a note to indicate that only a version bump has occurred.
     if (!newEntry.split("\n").some(line => line.startsWith("*"))) {
-      return dedent(`
-        ${newEntry}
+      // Add a note to indicate that only a version bump has occurred.
+      // TODO: actually list the dependencies that were bumped
+      const message = `**Note:** Version bump only for package ${pkg.name}`;
 
-        **Note:** Version bump only for package ${pkg.name}
-        `);
+      // the extra blank lines preserve the whitespace delimiting releases
+      return [newEntry.trim(), message, BLANK_LINE].join(BLANK_LINE);
     }
 
     return newEntry;
