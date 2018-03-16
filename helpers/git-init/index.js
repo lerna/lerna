14fcc1b5646c13b6fd03259553f68d5d1af70788
Daniel Stockman <daniel.stockman@gmail.com>
2018-03-15 19:02:29 -0700

    test(git-init): set user.name/email directly during fixture init

--- helpers/git-init/index.js
+++ helpers/git-init/index.js
@@ -5,7 +5,8 @@ const execa = require("execa");
 module.exports = gitInit;
 
 function gitInit(cwd, ...args) {
-  return execa("git", ["init", ...args], { cwd }).then(() =>
-    execa("git", ["config", "commit.gpgSign", "false"], { cwd })
-  );
+  return execa("git", ["init", ...args], { cwd })
+    .then(() => execa("git", ["config", "commit.gpgSign", "false"], { cwd }))
+    .then(() => execa("git", ["config", "user.email", "test@example.com"], { cwd }))
+    .then(() => execa("git", ["config", "user.name", "Tester McPerson"], { cwd }));
 }
