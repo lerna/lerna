ed5d2ba9f5503b0a986c755467887a82388efefa
Daniel Stockman <daniel.stockman@gmail.com>
2018-03-18 09:16:01 -0700

    test(windows): Avoid uncaught mocked rejection

--- utils/create-symlink/__tests__/create-symlink.test.js
+++ utils/create-symlink/__tests__/create-symlink.test.js
@@ -12,9 +12,11 @@ const createSymlink = require("..");
 const linkRelative = (from, to) => path.relative(path.dirname(to), from);
 
 describe("create-symlink", () => {
-  fs.lstat.mockRejectedValue("ENOENT");
+  fs.lstat.mockImplementation(() => Promise.reject(new Error("MOCK")));
   fs.unlink.mockResolvedValue();
   fs.symlink.mockResolvedValue();
+  // cmdShim is a traditional errback
+  cmdShim.mockImplementation(callsBack());
 
   if (process.platform !== "win32") {
     it("creates relative symlink to a directory", async () => {
@@ -44,7 +46,7 @@ describe("create-symlink", () => {
       const dst = path.resolve("./packages/package-1/node_modules/package-2");
       const type = "junction"; // even in posix environments :P
 
-      fs.lstat.mockResolvedValueOnce(); // something _does_ exist at destination
+      fs.lstat.mockImplementationOnce(() => Promise.resolve()); // something _does_ exist at destination
 
       await createSymlink(src, dst, type);
 
@@ -57,13 +59,20 @@ describe("create-symlink", () => {
       const dst = path.resolve("./packages/package-1/node_modules/.bin/package-2");
       const type = "exec";
 
-      // cmdShim is a traditional errback
-      cmdShim.mockImplementationOnce(callsBack());
-
       await createSymlink(src, dst, type);
 
       expect(fs.lstat).not.toBeCalled();
       expect(cmdShim).lastCalledWith(src, dst, expect.any(Function));
+    });
+
+    it("rejects when cmd-shim errors", async () => {
+      cmdShim.mockImplementationOnce(callsBack(new Error("yikes")));
+
+      try {
+        await createSymlink("src", "dst", "exec");
+      } catch (err) {
+        expect(err.message).toBe("yikes");
+      }
     });
 
     it("always uses absolute paths when creating symlinks", async () => {
@@ -71,7 +80,7 @@ describe("create-symlink", () => {
       const dst = path.resolve("./packages/package-1/node_modules/package-2");
       const type = "junction"; // only _actually_ matters in windows
 
-      fs.lstat.mockResolvedValueOnce(); // something _does_ exist at destination
+      fs.lstat.mockImplementationOnce(() => Promise.resolve()); // something _does_ exist at destination
 
       await createSymlink(src, dst, type);
 
