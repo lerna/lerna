89f9d3b4acb029bf4e21fa7b49f0fd90876bfe04
Daniel Stockman <daniel.stockman@gmail.com>
2018-03-18 08:52:47 -0700

    fix(clean-stack): Try to avoid causing errors during error cleanup

--- core/command/lib/clean-stack.js
+++ core/command/lib/clean-stack.js
@@ -3,9 +3,14 @@
 module.exports = cleanStack;
 
 function cleanStack(err, className) {
-  const lines = err.stack ? err.stack.split("\n") : err.split("\n");
+  const lines = err.stack ? err.stack.split("\n") : String(err).split("\n");
   const cutoff = new RegExp(`^    at ${className}.runCommand .*$`);
   const relevantIndex = lines.findIndex(line => cutoff.test(line));
 
-  return lines.slice(0, relevantIndex).join("\n");
+  if (relevantIndex) {
+    return lines.slice(0, relevantIndex).join("\n");
+  }
+
+  // nothing matched, just return original error
+  return err;
 }
