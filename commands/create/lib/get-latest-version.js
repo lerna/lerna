e6ddc2c8eb648ec2d019f0bc03111bf1fae2907a
Daniel Stockman <daniel.stockman@gmail.com>
2018-03-19 12:31:00 -0700

    add TODO in get-latest-version

--- commands/create/lib/get-latest-version.js
+++ commands/create/lib/get-latest-version.js
@@ -6,5 +6,6 @@ module.exports = getLatestVersion;
 
 // istanbul ignore next
 function getLatestVersion(depName, execOpts) {
+  // TODO: use pacote.manifest() instead
   return ChildProcessUtilities.execSync("npm", ["info", depName, "version"], execOpts);
 }
