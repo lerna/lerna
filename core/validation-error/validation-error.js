6201d0b73a735372f093b43b40fd02ed0ae0e24e
Daniel Stockman <daniel.stockman@gmail.com>
2018-03-26 15:55:10 -0700

    refactor(validation-error): Pass along remaining arguments to logger

--- core/validation-error/validation-error.js
+++ core/validation-error/validation-error.js
@@ -3,12 +3,12 @@
 const log = require("npmlog");
 
 class ValidationError extends Error {
-  constructor(prefix, message) {
+  constructor(prefix, message, ...rest) {
     super(message);
     this.name = "ValidationError";
     this.prefix = prefix;
     log.resume(); // might be paused, noop otherwise
-    log.error(prefix, message);
+    log.error(prefix, message, ...rest);
   }
 }
 
