881bc2a44702bd7a592c3dada4037fc2952f4ebc
Daniel Stockman <daniel.stockman@gmail.com>
2018-04-17 18:49:59 -0700

    test(helpers): Switch to new snapshot serializer API

--- helpers/show-commit/index.js
+++ helpers/show-commit/index.js
@@ -1,12 +1,12 @@
 "use strict";
 
 const execa = require("execa");
-const serializeGitSHA = require("@lerna-test/serialize-git-sha");
+const gitSHA = require("@lerna-test/serialize-git-sha");
 
 module.exports = showCommit;
 
 function showCommit(cwd, ...args) {
   return execa
     .stdout("git", ["show", "--unified=0", "--ignore-space-at-eol", "--pretty=%B%+D", ...args], { cwd })
-    .then(stdout => serializeGitSHA.print(stdout));
+    .then(stdout => gitSHA.serialize(stdout));
 }
