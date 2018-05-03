9cebed17c8214af2150a824499a8c343615ccc36
Bo Chen <bochen2014@yahoo.com>
2018-05-04 02:16:51 +1000

    fix(publish): Include all packages during global major bump (#1391)
    Fixes #1383

--- commands/publish/index.js
+++ commands/publish/index.js
@@ -40,6 +40,22 @@ function factory(argv) {
   return new PublishCommand(argv);
 }
 
+function isBreakingChange(currentVersion, nextVersion) {
+  const releaseType = semver.diff(currentVersion, nextVersion);
+  switch (releaseType) { //eslint-disable-line
+    case "major":
+      return true;
+    case "minor":
+      return semver.lt(currentVersion, "1.0.0");
+    case "patch":
+      return semver.lt(currentVersion, "0.1.0");
+    case "premajor":
+    case "preminor":
+    case "prepatch":
+    case "prerelease":
+      return false;
+  }
+}
 class PublishCommand extends Command {
   get defaultOptions() {
     return Object.assign({}, super.defaultOptions, {
@@ -147,7 +163,33 @@ class PublishCommand extends Command {
     const tasks = [
       () => this.getVersionsForUpdates(),
       versions => {
-        this.updatesVersions = versions;
+        if (
+          this.project.isIndependent() ||
+          versions.size === this.filteredPackages.size ||
+          this.options.canary
+        ) {
+          // independent, force-publish=*, or canary, we don't bump all
+          this.updatesVersions = versions;
+        } else {
+          let hasBreakingChange;
+          for (const [name, bump] of versions) {
+            hasBreakingChange =
+              hasBreakingChange || isBreakingChange(this.packageGraph.get(name).version, bump);
+          }
+          if (hasBreakingChange) {
+            const packages =
+              this.filteredPackages.length === this.packageGraph.size
+                ? this.packageGraph
+                : new Map(this.filteredPackages.map(({ name }) => [name, this.packageGraph.get(name)]));
+            this.updates = Array.from(packages.values());
+            this.updatesVersions = new Map();
+            this.updates.forEach(pkg => {
+              this.updatesVersions.set(pkg.name, this.globalVersion);
+            });
+          } else {
+            this.updatesVersions = versions;
+          }
+        }
       },
       () => this.confirmVersions(),
     ];
