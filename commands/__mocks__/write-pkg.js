39ce85c3d8c4063664e06a8e085b910ea150af7c
Daniel Stockman <daniel.stockman@gmail.com>
2018-05-29 12:21:44 -0700

    chore: Add helper methods to write-pkg mock

--- commands/__mocks__/write-pkg.js
+++ commands/__mocks__/write-pkg.js
@@ -10,6 +10,19 @@ const mockWritePkg = jest.fn((fp, data) => {
   return writePkg(fp, data);
 });
 
+const updatedManifest = name => registry.get(name);
+
+// a convenient format for assertions
+function updatedVersions() {
+  const result = {};
+
+  registry.forEach((pkg, name) => {
+    result[name] = pkg.version;
+  });
+
+  return result;
+}
+
 // keep test data isolated
 afterEach(() => {
   registry.clear();
@@ -17,3 +30,5 @@ afterEach(() => {
 
 module.exports = mockWritePkg;
 module.exports.registry = registry;
+module.exports.updatedManifest = updatedManifest;
+module.exports.updatedVersions = updatedVersions;
