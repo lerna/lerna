1116a4a08a2c32709f78f6201d9a499551da2266
Daniel Stockman <daniel.stockman@gmail.com>
2018-05-29 15:25:09 -0700

    chore: Use exponential notation in integration test timeout setup

--- setup-integration-timeout.js
+++ setup-integration-timeout.js
@@ -1,4 +1,4 @@
 "use strict";
 
-// allow CLI integration tests to run for awhile
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 300000;
+// allow CLI integration tests to run for awhile (300s)
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 300e3;
