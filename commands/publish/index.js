2955d3555221d5ce112f456a03fe4868049c0c80
Diego <dval@salesforce.com>
2018-09-08 15:13:06 -0700

    fix: allo forcePublish when using canary option

--- commands/publish/index.js
+++ commands/publish/index.js
@@ -195,7 +195,7 @@ class PublishCommand extends Command {
   }
 
   detectCanaryVersions() {
-    const { bump = "prepatch", preid = "alpha", tagVersionPrefix = "v", ignoreChanges } = this.options;
+    const { bump = "prepatch", preid = "alpha", tagVersionPrefix = "v", ignoreChanges, forcePublish } = this.options;
     // "prerelease" and "prepatch" are identical, for our purposes
     const release = bump.startsWith("pre") ? bump.replace("release", "patch") : `pre${bump}`;
 
@@ -210,6 +210,7 @@ class PublishCommand extends Command {
         bump: "prerelease",
         canary: true,
         ignoreChanges,
+        forcePublish
       })
     );
 
