a379266609550777c0177e560c4340e3cd116633
Daniel Stockman <daniel.stockman@gmail.com>
2018-09-11 13:35:25 -0700

    chore(helpers): Normalize newlines to coddle windows

--- helpers/normalize-test-root/index.js
+++ helpers/normalize-test-root/index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const normalizeNewline = require("normalize-newline");
 const normalizePath = require("normalize-path");
 const path = require("path");
 
@@ -10,7 +11,7 @@ const TEMP_DIR_REGEXP = /([^\s"]*[\\/][0-9a-f]{32})([^\s"]*)/g;
 // the excluded quotes are due to other snapshot serializers mutating the raw input
 
 function normalizeTestRoot(str) {
-  return str.replace(TEMP_DIR_REGEXP, serializeTestRoot);
+  return normalizeNewline(str).replace(TEMP_DIR_REGEXP, serializeTestRoot);
 }
 
 function serializeTestRoot(match, cwd, subPath) {
