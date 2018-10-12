349d43a5b23fddd6235c36ce5c68b92c8770cb75
Daniel Stockman <daniel.stockman@gmail.com>
2018-10-12 09:38:09 -0700

    build: Set LC_ALL env var in setup file, not shell

--- helpers/set-npm-userconfig/index.js
+++ helpers/set-npm-userconfig/index.js
@@ -3,3 +3,6 @@
 // Overwrite npm userconfig to avoid test pollution
 // https://docs.npmjs.com/misc/config#npmrc-files
 process.env.npm_config_userconfig = require("path").resolve(__dirname, "test-user.ini");
+
+// use consistent locale for all tests
+process.env.LC_ALL = "en_US.UTF-8";
