433162b22a05992cd57d768a92086975ba6e88da
Daniel Stockman <daniel.stockman@gmail.com>
2018-10-12 09:29:56 -0700

    build(travis): Enable windows CI

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@
 
 module.exports = {
   clearMocks: true,
+  // windows ci is terribly slow, so let's not burden it with coverage
+  collectCoverage: process.env.CI && process.env.TRAVIS_OS_NAME !== "windows",
   collectCoverageFrom: ["{commands,core,utils}/**/*.js", "!commands/create/lerna-module-data.js"],
   modulePathIgnorePatterns: ["/__fixtures__/"],
   roots: ["<rootDir>/commands", "<rootDir>/core", "<rootDir>/utils"],
@@ -9,4 +11,5 @@ module.exports = {
   setupTestFrameworkScriptFile: "<rootDir>/setup-unit-test-timeout.js",
   testEnvironment: "node",
   testRunner: "jest-circus/runner",
+  verbose: !!process.env.CI,
 };
