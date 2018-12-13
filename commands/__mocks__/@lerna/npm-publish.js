d02d1e13733b0408e0d10d946ca2ada65e6eb962
Daniel Stockman <daniel.stockman@gmail.com>
2018-12-12 17:37:51 -0800

    refactor(npm-publish): Remove npmPack() subexport, replaced by @lerna/pack-directory

--- commands/__mocks__/@lerna/npm-publish.js
+++ commands/__mocks__/@lerna/npm-publish.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const packed = new Set();
 const registry = new Map();
 
 // by default, act like a spy that populates registry
@@ -10,21 +9,6 @@ const mockNpmPublish = jest.fn((pkg, tag) => {
   return Promise.resolve(pkg);
 });
 
-const mockNpmPack = jest.fn((rootManifest, packages) => {
-  packages.forEach(pkg => {
-    packed.add(pkg.name);
-
-    // simulate decoration after npm pack
-    pkg.tarball = {
-      filename: `${pkg.name}-MOCKED.tgz`,
-    };
-  });
-
-  return Promise.resolve(packages.slice());
-});
-
-const mockMakePacker = jest.fn(rootManifest => batch => mockNpmPack(rootManifest, batch));
-
 // a convenient format for assertions
 function order() {
   return Array.from(registry.keys());
@@ -33,12 +17,8 @@ function order() {
 // keep test data isolated
 afterEach(() => {
   registry.clear();
-  packed.clear();
 });
 
 module.exports = mockNpmPublish;
-module.exports.npmPack = mockNpmPack;
-module.exports.makePacker = mockMakePacker;
 module.exports.order = order;
-module.exports.packed = packed;
 module.exports.registry = registry;
--- utils/npm-publish/npm-publish.js
+++ utils/npm-publish/npm-publish.js
@@ -3,16 +3,11 @@
 const fs = require("fs-extra");
 const log = require("libnpm/log");
 const path = require("path");
-const pMap = require("p-map");
 
 const ChildProcessUtilities = require("@lerna/child-process");
 const getExecOpts = require("@lerna/get-npm-exec-opts");
-const hasNpmVersion = require("@lerna/has-npm-version");
-const logPacked = require("@lerna/log-packed");
 
 module.exports = npmPublish;
-module.exports.npmPack = npmPack;
-module.exports.makePacker = makePacker;
 
 function npmPublish(pkg, tag, { npmClient, registry }) {
   log.verbose("publish", pkg.name);
@@ -40,126 +35,4 @@ function npmPublish(pkg, tag, { npmClient, registry }) {
     // don't leave the generated tarball hanging around after success
     fs.remove(path.join(pkg.location, pkg.tarball.filename)).then(() => pkg)
   );
-}
-
-function makePackOptions(rootManifest) {
-  const opts = getExecOpts(rootManifest);
-
-  // let that juicy npm logging have its day in the sun
-  opts.stdio = ["ignore", "pipe", "inherit"];
-
-  // no need for fancy error wrapping
-  delete opts.pkg;
-
-  if (hasNpmVersion(">=5.10.0")) {
-    // request JSON output for easier parsing of filenames
-    opts.env.npm_config_json = true;
-  }
-
-  /* istanbul ignore if */
-  if (process.env.LERNA_INTEGRATION) {
-    // override process.env.npm_config_dry_run from integration tests
-    opts.env.npm_config_dry_run = false;
-  }
-
-  log.silly("exec", "npm options", opts);
-  return opts;
-}
-
-function npmPack(rootManifest, packages, opts = makePackOptions(rootManifest)) {
-  // NOTE: All of the istanbul-ignored branches are covered in integration tests
-  log.verbose("pack", packages.map(p => p.name));
-
-  const args = ["pack"].concat(packages.map(p => p.location));
-
-  log.silly("exec", "npm", args);
-  const proc = ChildProcessUtilities.exec("npm", args, opts);
-
-  // --json output is an array of objects stringified with 2-space indent
-  const jsonStart = /^\[\n[ ]{2}{\n[ ]{4}/;
-
-  let jsonBegan = false;
-  let jsonString = "";
-
-  // npm-lifecycle log note should be removed when --loglevel=silent
-  // we cannot pass --silent because it also hides --json
-  const isNote = /^\n> /;
-  /* istanbul ignore next */
-  const output =
-    log.level !== "silent"
-      ? chunk => process.stdout.write(chunk)
-      : chunk => !isNote.test(chunk) && process.stdout.write(chunk);
-
-  // only pipe script output instead of inheriting stdout
-  // stop piping when we reach the start of --json output
-  proc.stdout.setEncoding("utf8");
-  proc.stdout.on("data", chunk => {
-    if (jsonBegan) {
-      // it could be larger than a single chunk
-      jsonString += chunk;
-    } else if (jsonStart.test(chunk)) {
-      // the first json chunk (commonly the only one)
-      jsonString += chunk;
-      // stop writing output to stdout
-      jsonBegan = true;
-    } else {
-      // maybe write non-json chunk to stdout
-      log.pause();
-      output(chunk);
-      log.resume();
-    }
-  });
-
-  return proc.then(result => {
-    const tarballs = jsonString ? JSON.parse(jsonString) : parseLegacyTarballs(result, packages);
-
-    tarballs.forEach(logPacked);
-
-    // npm pack --json list is in the same order as the packages input
-    for (let i = 0; i < packages.length; i += 1) {
-      // could do this inside the mapper, but we need this metadata regardless of p-map success
-      const pkg = packages[i];
-
-      // instead of complicated copypasta, use literally what npm did
-      pkg.tarball = tarballs[i];
-    }
-
-    return pMap(
-      packages,
-      pkg => {
-        const inRoot = path.join(pkg.rootPath, pkg.tarball.filename);
-        const toLeaf = path.join(pkg.location, pkg.tarball.filename);
-
-        return fs.move(inRoot, toLeaf, { overwrite: true }).then(() => pkg);
-      },
-      { concurrency: 10 }
-    );
-  });
-}
-
-function makePacker(rootManifest) {
-  const opts = makePackOptions(rootManifest);
-
-  return packages => npmPack(rootManifest, packages, opts);
-}
-
-function parseLegacyTarballs(result, packages) {
-  // legacy `npm pack` outputs the generated tarball names
-  // at the end of stdout in the order of package input(s)
-  const isTgz = /^[\S]+\.tgz$/;
-  const lines = result.stdout.split("\n");
-  const files = lines.filter(line => isTgz.test(line));
-
-  // each result is passed to log-packed, so it needs decoration
-  return files.map((filename, idx) => {
-    const pkg = packages[idx];
-
-    return {
-      // the important part
-      filename,
-      // make log-packed show _something_ useful
-      name: pkg.name,
-      version: pkg.version,
-    };
-  });
 }
