3359c637d12ebf7a5ec228395375794e61972c65
Daniel Stockman <daniel.stockman@gmail.com>
2018-12-19 14:28:51 -0800

    fix: Add pulse-till-done utility

---
+++ utils/pulse-till-done/pulse-till-done.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const log = require("libnpm/log");
+
+let pulsers = 0;
+let pulse;
+
+function pulseStart(prefix) {
+  pulsers += 1;
+
+  if (pulsers > 1) {
+    return;
+  }
+
+  pulse = setInterval(() => log.gauge.pulse(prefix), 150);
+}
+
+function pulseStop() {
+  pulsers -= 1;
+
+  if (pulsers > 0) {
+    return;
+  }
+
+  clearInterval(pulse);
+}
+
+function pulseTillDone(prefix, promise) {
+  if (!promise) {
+    /* eslint-disable no-param-reassign */
+    promise = prefix;
+    prefix = "";
+    /* eslint-enable no-param-reassign */
+  }
+
+  pulseStart(prefix);
+
+  return Promise.resolve(promise).then(
+    val => {
+      pulseStop();
+      return val;
+    },
+    err => {
+      pulseStop();
+      throw err;
+    }
+  );
+}
+
+module.exports = pulseTillDone;
