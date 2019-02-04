ace1036a69a4aa0ddf0cd988e887104a452ec491
Daniel Stockman <daniel.stockman@gmail.com>
2019-02-04 14:11:05 -0800

    chore(test): Upgrade Jest to v24

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   modulePathIgnorePatterns: ["/__fixtures__/"],
   roots: ["<rootDir>/commands", "<rootDir>/core", "<rootDir>/utils"],
   setupFiles: ["@lerna-test/silence-logging", "@lerna-test/set-npm-userconfig"],
-  setupTestFrameworkScriptFile: "<rootDir>/setup-unit-test-timeout.js",
+  setupFilesAfterEnv: ["<rootDir>/setup-unit-test-timeout.js"],
   testEnvironment: "node",
   testRunner: "jest-circus/runner",
   verbose: !!process.env.CI,
--- jest.integration.js
+++ jest.integration.js
@@ -5,7 +5,7 @@ module.exports = {
   modulePathIgnorePatterns: ["/__fixtures__/"],
   roots: ["<rootDir>/integration"],
   setupFiles: ["@lerna-test/set-npm-userconfig"],
-  setupTestFrameworkScriptFile: "<rootDir>/setup-integration-timeout.js",
+  setupFilesAfterEnv: ["<rootDir>/setup-integration-timeout.js"],
   snapshotSerializers: ["@lerna-test/serialize-placeholders"],
   testEnvironment: "node",
   testRunner: "jest-circus/runner",
