42e4a5c6bfd9fac6550b6b7d1efe796c3a8c39db
Daniel Stockman <daniel.stockman@gmail.com>
2019-02-05 11:44:07 -0800

    fix(collect-updates): Improve logging, making ignored globs explicit

--- utils/collect-updates/lib/make-diff-predicate.js
+++ utils/collect-updates/lib/make-diff-predicate.js
@@ -9,21 +9,39 @@ const slash = require("slash");
 module.exports = makeDiffPredicate;
 
 function makeDiffPredicate(committish, execOpts, ignorePatterns = []) {
-  const ignoreFilters = new Set(ignorePatterns.map(p => minimatch.filter(`!${p}`, { matchBase: true })));
+  const ignoreFilters = new Set(
+    ignorePatterns.map(p =>
+      minimatch.filter(`!${p}`, {
+        matchBase: true,
+      })
+    )
+  );
+
+  if (ignoreFilters.size) {
+    log.info("ignoring diff in paths matching", ignorePatterns);
+  }
 
   return function hasDiffSinceThatIsntIgnored(node) {
     const diff = diffSinceIn(committish, node.location, execOpts);
 
     if (diff === "") {
+      log.silly("", "no diff found in %s", node.name);
       return false;
     }
 
+    log.silly("found diff in", diff);
     let changedFiles = diff.split("\n");
 
     if (ignoreFilters.size) {
       for (const ignored of ignoreFilters) {
         changedFiles = changedFiles.filter(ignored);
       }
+    }
+
+    if (changedFiles.length) {
+      log.verbose("filtered diff", changedFiles);
+    } else {
+      log.verbose("", "no diff found in %s (after filtering)", node.name);
     }
 
     return changedFiles.length > 0;
@@ -39,10 +57,6 @@ function diffSinceIn(committish, location, opts) {
     args.push("--", formattedLocation);
   }
 
-  log.silly("diffSinceIn", committish, formattedLocation);
-
-  const diff = childProcess.execSync("git", args, opts);
-  log.silly("diff", diff);
-
-  return diff;
+  log.silly("checking diff", formattedLocation);
+  return childProcess.execSync("git", args, opts);
 }
