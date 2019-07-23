2007125872ee07ea68602b364c52fe3815d216ba
Daniel Stockman <daniel.stockman@gmail.com>
2019-07-23 15:05:49 -0700

    test(pack-directory): Convert tests to use proper fixture

--- utils/pack-directory/__tests__/pack-directory.test.js
+++ utils/pack-directory/__tests__/pack-directory.test.js
@@ -3,14 +3,12 @@
 // actually _run_ the lifecycles, gorrammit
 jest.unmock("@lerna/run-lifecycle");
 
-const fs = require("fs-extra");
 const path = require("path");
 const normalizePath = require("normalize-path");
 const { printObjectProperties } = require("pretty-format/build/collections");
 const npmConf = require("@lerna/npm-conf");
-const Package = require("@lerna/package");
 const { getPackages } = require("@lerna/project");
-const initFixture = require("@lerna-test/init-fixture")(path.resolve(__dirname, "../../../integration"));
+const initFixture = require("@lerna-test/init-fixture")(__dirname);
 
 const packDirectory = require("..");
 
@@ -81,7 +79,7 @@ expect.addSnapshotSerializer({
 
 describe("pack-directory", () => {
   it("resolves tarball metadata objects on success", async () => {
-    const cwd = await initFixture("lerna-bootstrap");
+    const cwd = await initFixture("pack-directory");
     const conf = npmConf({ prefix: cwd }).snapshot;
     const pkgs = await getPackages(cwd);
 
@@ -94,130 +92,139 @@ describe("pack-directory", () => {
     const SHASUM_PATTERN = /[0-9a-f]{40}/;
 
     expect(head).toMatchInlineSnapshot(`
-Object {
-  "bundled": Array [],
-  "entryCount": 2,
-  "filename": "integration-package-1-1.0.0.tgz",
-  "files": Array [
-    Object {
-      "mode": "MODE",
-      "path": "dist/index.js",
-      "size": 141,
-    },
-    Object {
-      "mode": "MODE",
-      "path": "package.json",
-      "size": 325,
-    },
-  ],
-  "id": "@integration/package-1@1.0.0",
-  "integrity": "INTEGRITY",
-  "name": "@integration/package-1",
-  "shasum": "SHASUM",
-  "size": "TAR_SIZE",
-  "tarFilePath": "__TMP_DIR__/integration-package-1-1.0.0.tgz",
-  "unpackedSize": 466,
-  "version": "1.0.0",
-}
-`);
+      Object {
+        "bundled": Array [],
+        "entryCount": 2,
+        "filename": "scope-head-1.0.0.tgz",
+        "files": Array [
+          Object {
+            "mode": "MODE",
+            "path": "dist/index.js",
+            "size": 37,
+          },
+          Object {
+            "mode": "MODE",
+            "path": "package.json",
+            "size": 159,
+          },
+        ],
+        "id": "@scope/head@1.0.0",
+        "integrity": "INTEGRITY",
+        "name": "@scope/head",
+        "shasum": "SHASUM",
+        "size": "TAR_SIZE",
+        "tarFilePath": "__TMP_DIR__/scope-head-1.0.0.tgz",
+        "unpackedSize": 196,
+        "version": "1.0.0",
+      }
+    `);
     // integrity is an instance of Integrity
     // https://github.com/zkat/ssri/blob/a4337cd672f341deee2b52699b6720d82e4d0ddf/index.js#L83
     expect(head.integrity.toString()).toMatch(INTEGRITY_PATTERN);
     expect(head.shasum).toMatch(SHASUM_PATTERN);
 
     expect(tail).toMatchInlineSnapshot(`
-Object {
-  "bundled": Array [],
-  "entryCount": 1,
-  "filename": "package-4-1.0.0.tgz",
-  "files": Array [
-    Object {
-      "mode": "MODE",
-      "path": "package.json",
-      "size": 224,
-    },
-  ],
-  "id": "package-4@1.0.0",
-  "integrity": "INTEGRITY",
-  "name": "package-4",
-  "shasum": "SHASUM",
-  "size": "TAR_SIZE",
-  "tarFilePath": "__TMP_DIR__/package-4-1.0.0.tgz",
-  "unpackedSize": 224,
-  "version": "1.0.0",
-}
-`);
+      Object {
+        "bundled": Array [],
+        "entryCount": 2,
+        "filename": "tail-4.0.0.tgz",
+        "files": Array [
+          Object {
+            "mode": "MODE",
+            "path": "index.js",
+            "size": 37,
+          },
+          Object {
+            "mode": "MODE",
+            "path": "package.json",
+            "size": 131,
+          },
+        ],
+        "id": "tail@4.0.0",
+        "integrity": "INTEGRITY",
+        "name": "tail",
+        "shasum": "SHASUM",
+        "size": "TAR_SIZE",
+        "tarFilePath": "__TMP_DIR__/tail-4.0.0.tgz",
+        "unpackedSize": 168,
+        "version": "4.0.0",
+      }
+    `);
     expect(tail.integrity.toString()).toMatch(INTEGRITY_PATTERN);
     expect(tail.shasum).toMatch(SHASUM_PATTERN);
 
-    const mid = pkgs.pop();
-    const json = Object.assign(mid.toJSON(), {
-      name: "package-3",
-      // omit scripts directory
-      files: ["*.js"],
-      // overwrite existing postinstall + test
-      scripts: {
-        prepublish: "exit 1",
-        prepublishOnly: "node scripts/build.js",
-      },
-    });
-
-    await Promise.all([
-      fs.writeJSON(mid.manifestLocation, json, { spaces: 2 }),
-      fs.outputFile(
-        path.join(mid.location, "scripts", "build.js"),
-        // Windows requires this ridiculous workaround
-        // because echoing a file without CRLF garbage is apparently too hard :P
-        "require('fs').writeFileSync(require('path').resolve('index.js'), 'badgerbadgerbadgerbadger');"
-      ),
-    ]);
-
-    const mushroom = new Package(json, mid.location, mid.rootPath);
-    const lazy = await packDirectory(
-      mushroom,
-      mushroom.location,
+    const next = pkgs.pop();
+    const pubs = await packDirectory(
+      next,
+      next.location,
       Object.assign({}, conf, {
         "ignore-prepublish": true,
         "lerna-command": "publish",
       })
     );
 
-    expect(lazy).toMatchInlineSnapshot(`
-Object {
-  "bundled": Array [],
-  "entryCount": 4,
-  "filename": "package-3-1.0.0.tgz",
-  "files": Array [
-    Object {
-      "mode": "MODE",
-      "path": "cli1.js",
-      "size": 108,
-    },
-    Object {
-      "mode": "MODE",
-      "path": "cli2.js",
-      "size": 108,
-    },
-    Object {
-      "mode": "MODE",
-      "path": "index.js",
-      "size": 24,
-    },
-    Object {
-      "mode": "MODE",
-      "path": "package.json",
-      "size": 465,
-    },
-  ],
-  "id": "package-3@1.0.0",
-  "integrity": "INTEGRITY",
-  "name": "package-3",
-  "shasum": "SHASUM",
-  "size": "TAR_SIZE",
-  "tarFilePath": "__TMP_DIR__/package-3-1.0.0.tgz",
-  "unpackedSize": 705,
-  "version": "1.0.0",
-}
-`);
+    expect(pubs).toMatchInlineSnapshot(`
+      Object {
+        "bundled": Array [],
+        "entryCount": 2,
+        "filename": "pubs-3.0.0.tgz",
+        "files": Array [
+          Object {
+            "mode": "MODE",
+            "path": "index.js",
+            "size": 21,
+          },
+          Object {
+            "mode": "MODE",
+            "path": "package.json",
+            "size": 171,
+          },
+        ],
+        "id": "pubs@3.0.0",
+        "integrity": "INTEGRITY",
+        "name": "pubs",
+        "shasum": "SHASUM",
+        "size": "TAR_SIZE",
+        "tarFilePath": "__TMP_DIR__/pubs-3.0.0.tgz",
+        "unpackedSize": 192,
+        "version": "3.0.0",
+      }
+    `);
+
+    const last = pkgs.pop();
+    const subs = await packDirectory(
+      last,
+      // TODO: use location
+      last.contents,
+      conf
+    );
+
+    expect(subs).toMatchInlineSnapshot(`
+      Object {
+        "bundled": Array [],
+        "entryCount": 2,
+        "filename": "subs-2.0.0.tgz",
+        "files": Array [
+          Object {
+            "mode": "MODE",
+            "path": "prepacked.js",
+            "size": 21,
+          },
+          Object {
+            "mode": "MODE",
+            "path": "package.json",
+            "size": 115,
+          },
+        ],
+        "id": "subs@2.0.0",
+        "integrity": "INTEGRITY",
+        "name": "subs",
+        "shasum": "SHASUM",
+        "size": "TAR_SIZE",
+        "tarFilePath": "__TMP_DIR__/subs-2.0.0.tgz",
+        "unpackedSize": 136,
+        "version": "2.0.0",
+      }
+    `);
   });
 });
