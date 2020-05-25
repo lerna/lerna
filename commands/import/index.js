e4f4fef5933fa37ce49363ef50dd08984f681e3e
Tim Shilov <tim@shilov.dev>
2020-05-25 17:34:44 +0300

    Code linted

--- commands/import/index.js
+++ commands/import/index.js
@@ -239,7 +239,7 @@ class ImportCommand extends Command {
         .catch(err => {
           // Getting commit diff to see if it's empty
           const diff = this.externalExecSync("git", ["diff", "-s", `${sha}^!`]).trim();
-          if (diff === '') {
+          if (diff === "") {
             tracker.completeWork(1);
 
             // Automatically skip empty commits
