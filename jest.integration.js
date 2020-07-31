61001ad4db94d62947e7ca2ad7fb0068995e3c76
Avi Vahl <avi.vahl@wix.com>
2020-07-31 05:23:59 +0300

    test: avoid windows-specific flow

--- jest.integration.js
+++ jest.integration.js
@@ -10,11 +10,3 @@ module.exports = {
   testRunner: "jest-circus/runner",
   verbose: true,
 };
-
-// split tests into smaller chunks because windows is agonizingly slow
-if (process.env.CI && process.env.TRAVIS_OS_NAME === "windows") {
-  module.exports.testMatch =
-    process.env.LERNA_CI_TYPE === "publish"
-      ? ["<rootDir>/integration/@(lerna-publish)*.test.js"]
-      : ["<rootDir>/integration/!(lerna-publish)*.test.js"];
-}
