927c72670dc991dde5193789f0f543b2ae2c3fca
Daniel Stockman <5605+evocateur@users.noreply.github.com>
2020-11-08 16:58:23 -0800

    test(helpers): Decouple clone-fixture from init-fixture

--- helpers/clone-fixture/index.js
+++ helpers/clone-fixture/index.js
@@ -3,7 +3,6 @@
 const execa = require("execa");
 const fileUrl = require("file-url");
 const tempy = require("tempy");
-const gitInit = require("@lerna-test/git-init");
 const initFactory = require("@lerna-test/init-fixture");
 
 module.exports = cloneFixture;
@@ -16,7 +15,7 @@ function cloneFixture(startDir) {
       const repoDir = tempy.directory();
       const repoUrl = fileUrl(repoDir, { resolve: false });
 
-      return gitInit(repoDir, "--bare")
+      return execa("git", ["init", "--bare"], { cwd: repoDir })
         .then(() => execa("git", ["remote", "add", "origin", repoUrl], { cwd }))
         .then(() => execa("git", ["push", "-u", "origin", "main"], { cwd }))
         .then(() => ({
