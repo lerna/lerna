b7d9bde7d633e2c4f207cbc423e2fb1e696c18ed
Daniel Stockman <5605+evocateur@users.noreply.github.com>
2020-11-11 17:47:11 -0800

    chore(dev-deps): find-up@^5.0.0

--- helpers/find-fixture/index.js
+++ helpers/find-fixture/index.js
@@ -6,8 +6,8 @@ const path = require("path");
 module.exports = findFixture;
 
 function findFixture(cwd, fixtureName) {
-  return findUp(path.join("__fixtures__", fixtureName), { cwd }).then(fixturePath => {
-    if (fixturePath === null) {
+  return findUp(path.join("__fixtures__", fixtureName), { cwd, type: "directory" }).then(fixturePath => {
+    if (fixturePath === undefined) {
       throw new Error(`Could not find fixture with name "${fixtureName}"`);
     }
 
