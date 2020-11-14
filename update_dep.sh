b8f060de9c843f3934edaa76d31e1199395059c4
Daniel Stockman <5605+evocateur@users.noreply.github.com>
2020-11-13 17:08:10 -0800

    chore: Add dependency update script

---
+++ update_dep.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# brew install ripgrep jq sponge
+
+TARGET="$1"
+
+echo "Updating package: '$TARGET'"
+
+LATEST=$(npm v "$TARGET" version)
+LATEST="^$LATEST"
+
+echo "...using version: '$LATEST'"
+
+INFILES=$(rg --sort=path --files-with-matches --glob package.json --fixed-strings '"'$TARGET'":')
+
+echo "located in files:"
+echo $INFILES
+echo
+
+for file in $INFILES; do
+    cat $file | \
+        jq \
+            --arg pkg "$TARGET" \
+            --arg ver "$LATEST" \
+            '.dependencies[$pkg] = $ver' \
+        | sponge $file;
+    echo "Finished editing: $file"
+done
+
+npm install
+git add -u .
+npm home $TARGET --no-browser
