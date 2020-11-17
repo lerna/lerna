7974b351e53575503dd881c639b53d820e4f525e
Daniel Stockman <5605+evocateur@users.noreply.github.com>
2020-11-17 15:14:02 -0800

    feat(deps): @evocateur/libnpmaccess -> libnpmaccess@^4.0.1

--- commands/publish/lib/verify-npm-package-access.js
+++ commands/publish/lib/verify-npm-package-access.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const access = require("@evocateur/libnpmaccess");
+const access = require("libnpmaccess");
 const pulseTillDone = require("@lerna/pulse-till-done");
 const ValidationError = require("@lerna/validation-error");
 const FetchConfig = require("./fetch-config");
