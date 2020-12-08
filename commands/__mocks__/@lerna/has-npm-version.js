56cba2ffc4dccf3380548567a36a35345fb7c747
Daniel Stockman <5605+evocateur@users.noreply.github.com>
2020-12-08 13:02:41 -0800

    feat(has-npm-version): Remove unused makePredicate() export
    BREAKING CHANGE: The makePredicate() export has been removed, memoization is now the responsibility of the caller.

--- commands/__mocks__/@lerna/has-npm-version.js
+++ commands/__mocks__/@lerna/has-npm-version.js
@@ -1,7 +1,5 @@
 "use strict";
 
 const mockHasNpmVersion = jest.fn(() => true);
-const mockMakePredicate = jest.fn(() => (range) => mockHasNpmVersion(range));
 
 module.exports.hasNpmVersion = mockHasNpmVersion;
-module.exports.makePredicate = mockMakePredicate;
--- utils/has-npm-version/lib/has-npm-version.js
+++ utils/has-npm-version/lib/has-npm-version.js
@@ -4,16 +4,9 @@ const semver = require("semver");
 const childProcess = require("@lerna/child-process");
 
 module.exports.hasNpmVersion = hasNpmVersion;
-module.exports.makePredicate = makePredicate;
 
 function hasNpmVersion(range) {
   return rangeSatisfies(getNpmVersion(), range);
-}
-
-function makePredicate() {
-  const npmVersion = getNpmVersion();
-
-  return (range) => rangeSatisfies(npmVersion, range);
 }
 
 function rangeSatisfies(npmVersion, range) {
