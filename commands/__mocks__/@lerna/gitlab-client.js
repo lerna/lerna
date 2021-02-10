63f18ba96153f352373eafb3202b3cc591ab4c8d
Daniel Stockman <5605+evocateur@users.noreply.github.com>
2021-02-09 17:39:00 -0800

    test(version): Assert on mocked releases, not calls

--- commands/__mocks__/@lerna/gitlab-client.js
+++ commands/__mocks__/@lerna/gitlab-client.js
@@ -1,9 +1,20 @@
 "use strict";
 
+const releases = new Map();
+
+// keep test data isolated
+afterEach(() => {
+  releases.clear();
+});
+
 const client = {
   repos: {
-    createRelease: jest.fn(),
+    createRelease: jest.fn((opts) => {
+      releases.set(opts.name, opts);
+      return Promise.resolve();
+    }),
   },
 };
 
-module.exports.createGitLabClient = () => client;
+module.exports.createGitLabClient = jest.fn(() => client);
+module.exports.createGitLabClient.releases = releases;
