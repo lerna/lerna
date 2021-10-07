dc67d22a13c2f314eebcf8bd9363f4a5538aac8d
blushingpenguin <mark@blushingpenguin.com>
2021-10-07 19:38:19 +0100

    #2973: fix overall process exiting with error code 128 if git push --atomic fails

--- commands/version/lib/git-push.js
+++ commands/version/lib/git-push.js
@@ -23,6 +23,10 @@ function gitPush(remote, branch, opts) {
         /atomic/.test(error.stderr) ||
         (process.env.GIT_REDIRECT_STDERR === "2>&1" && /atomic/.test(error.stdout))
       ) {
+        // childProcess.exec has propagated the error code to the process exit code --
+        // we'll clear it here as it will not propagate a success code
+        process.exitCode = 0;
+
         // --atomic is only supported in git >=2.4.0, which some crusty CI environments deem unnecessary to upgrade.
         // so let's try again without attempting to pass an option that is almost 5 years old as of this writing...
         log.warn("gitPush", error.stderr);
