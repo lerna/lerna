5ca6d589f6546a3791d9c90487c39de216aedf66
“JamesHenry” <james@henry.sc>
2022-05-23 22:21:22 +0400

    chore: try increasing unit test timeout to address windows flakiness

--- setup-unit-test-timeout.js
+++ setup-unit-test-timeout.js
@@ -1,4 +1,4 @@
 "use strict";
 
-// allow 15s timeout in an attempt to lessen flakiness of unit tests...
-jest.setTimeout(15e3);
+// allow 30s timeout in an attempt to lessen flakiness of unit tests...
+jest.setTimeout(30e3);
