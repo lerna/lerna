2f171c6b95f6adb922e8ce10e3b795aa92d96226
ben <3447705+bcabanes@users.noreply.github.com>
2022-06-08 12:43:11 -0400

    feat(website): add documentation website

---
+++ website/src/components/powered-by-nx.tsx
@@ -0,0 +1,37 @@
+import clsx from "clsx";
+import React from "react";
+import styles from "./powered-by-nx.module.css";
+
+export default function PoweredByNx(): JSX.Element {
+  return (
+    <section className="padding-vert--xl container">
+      <div className={clsx("row row--no-gutters shadow--md", styles.item)}>
+        <div
+          className="col col--6"
+          aria-hidden="true"
+          style={{
+            backgroundImage: "url('/images/background/rocket.avif')",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
+        ></div>
+        <div className={clsx("col col--6", styles.item__inner)}>
+          <div>
+            <h1 className={clsx("margin-bottom--md", styles.item__title)}>
+              Fastest Build System <span>Powered by Nx</span>
+            </h1>
+            <p className="margin-bottom--md">
+              Lerna runs a command against any number of projects in the most efficient way, in the right
+              order, in parallel, using advanced caching and with the possibility to distribute that on
+              multiple machines.
+            </p>
+            <a className="button button--secondary" href="/docs/core-concepts/running-tasks">
+              Running Tasks
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ website/src/components/publish-with-lerna.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+import React from "react";
+import styles from "./publish-with-lerna.module.css";
+
+export default function PublishWithLerna(): JSX.Element {
+  return (
+    <section className="padding-vert--xl container">
+      <div className={clsx("row row--no-gutters shadow--md", styles.item)}>
+        <div className={clsx("col col--6", styles.item__inner)}>
+          <div>
+            <h1 className="margin-bottom--md">Ultimate Tool for Publishing Packages</h1>
+            <p className="margin-bottom--md">
+              Lerna is the ultimate tool for publishing multiple packages to npm. Whether the packages have
+              independent versions or not, Lerna's got you covered.
+            </p>
+            <a className="button button--secondary" href="/docs/core-concepts/versioning-and-publishing">
+              Publishing Packages
+            </a>
+          </div>
+        </div>
+        <div
+          className="col col--6"
+          aria-hidden="true"
+          style={{
+            backgroundImage: "url('/images/background/parcel.avif')",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "right",
+          }}
+        ></div>
+      </div>
+    </section>
+  );
+}
