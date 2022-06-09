72305e4dbab607a2d87ae4efa6ee577c93a9dda9
James Henry <james@henry.sc>
2022-06-09 12:16:17 +0400

    fix: allow maintenance LTS node 14 engines starting at 14.15.0 (#3161)

--- setup-unit-test-timeout.js
+++ setup-unit-test-timeout.js
@@ -1,4 +1,4 @@
 "use strict";
 
-// allow 30s timeout in an attempt to lessen flakiness of unit tests...
-jest.setTimeout(30e3);
+// allow 45s timeout in an attempt to lessen flakiness of unit tests...
+jest.setTimeout(45e3);
