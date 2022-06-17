4bc28bc1bce269c4ebad6b2b3b5222d773cd7879
“JamesHenry” <james@henry.sc>
2022-06-17 15:09:06 +0400

    chore: update workflow

--- .github/actions/update-historical/index.js
+++ .github/actions/update-historical/index.js
@@ -14,7 +14,7 @@ async function main() {
   const staleIssuesWithAnyUpdatesSinceLabelling = [];
   const staleIssuesToBeClosed = [];
   const STALE_LABEL_NAME = "stale";
-  const AUTOMATED_LABELLING_DATETIME = new Date("2022-06-03 19:13 GMT+4");
+  const AUTOMATED_LABELLING_DATETIME = new Date("2022-06-03 19:24 GMT+4");
 
   for await (const issuePage of octokit.paginate.iterator(octokit.rest.issues.listForRepo, {
     ...context.repo,
@@ -38,6 +38,10 @@ async function main() {
 
   for (const issue of allStaleIssues) {
     const updatedAt = new Date(issue.updated_at);
+    console.log({
+      AUTOMATED_LABELLING_DATETIME: AUTOMATED_LABELLING_DATETIME.getTime(),
+      updatedAt: updatedAt.getTime(),
+    });
 
     if (updatedAt.getTime() > AUTOMATED_LABELLING_DATETIME.getTime()) {
       staleIssuesWithAnyUpdatesSinceLabelling.push(issue);
