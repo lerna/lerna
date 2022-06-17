c4199157581921eecb53c0ff40f79b19f0ede08b
“JamesHenry” <james@henry.sc>
2022-06-17 16:13:32 +0400

    chore: update workflow

--- .github/actions/update-historical/index.js
+++ .github/actions/update-historical/index.js
@@ -57,6 +57,44 @@ async function main() {
   });
 
   console.log(JSON.stringify(staleIssuesToBeClosed, null, 2));
+
+  for (const isseToBeClosed of staleIssuesToBeClosed) {
+    await octokit.rest.issues.createComment({
+      ...context.repo,
+      issue_number: isseToBeClosed.number,
+      body: `Hi Folks 👋 
+
+You will have seen in our message above that we at Nrwl are working really hard to bring the lerna repo up to date with what matters most to its community in June 2022.
+
+As previously stated in that message, because we have not heard from the original author of this issue within the last 14 days, we are now automatically closing it.
+
+**If any users, including the original author, are still impacted by this issue then we still want to hear from you!**
+
+All we ask is that you first update to the latest lerna (\`5.1.4\` at the time of writing) to make sure it is still reproducible, and then fill out one of our new issue templates, providing all the requested details which apply to your situation:
+
+https://github.com/lerna/lerna/issues/new/choose
+
+Many thanks again! 🙏
+
+---
+
+P.S. Over and above getting to grips with the repo, we have also been hard at work launching a new website, resolving all vulnerabilities, merging exciting new features and reigniting community PR contributions! 🚀
+
+You can read our recent blog post to learn more about what we've been up to:
+https://blog.nrwl.io/lerna-5-1-new-website-new-guides-new-lerna-example-repo-distributed-caching-support-and-speed-64d66410bec7
+`,
+    });
+
+    await octokit.rest.issues.update({
+      ...context.repo,
+      issue_number: isseToBeClosed.number,
+      state: "closed",
+    });
+    await octokit.rest.issues.lock({
+      ...context.repo,
+      issue_number: isseToBeClosed.number,
+    });
+  }
 }
 
 main();
