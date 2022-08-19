711364c26a29c389fc73ca54a78ab59061b89c51
Austin Fahsl <austin@fahsl.io>
2022-08-19 16:17:58 -0400

    chore(e2e): add test to cover pnpm lockfile update skipping install scripts

--- e2e/tests/lerna-version/positional-arguments-pnpm.spec.ts
+++ e2e/tests/lerna-version/positional-arguments-pnpm.spec.ts
@@ -160,4 +160,75 @@ describe("lerna-version-positional-arguments-pnpm", () => {
       }
     `);
   });
+
+  it("should not run pnpm install lifecycle scripts, but still update pnpm-lock.yaml", async () => {
+    await fixture.updateJson("package.json", (json) => ({
+      ...json,
+      scripts: {
+        preinstall: "exit 1",
+        install: "exit 1",
+        postinstall: "exit 1",
+        prepublish: "exit 1",
+        prepare: "exit 1",
+      },
+    }));
+    await fixture.exec("git add package.json");
+    await fixture.exec("git commit -m 'Add failing lifecycle scripts'");
+
+    const output = await fixture.lerna("version 3.3.3 -y");
+
+    expect(output.combinedOutput).toMatchInlineSnapshot(`
+      lerna notice cli v999.9.9-e2e.0
+      lerna info current version 0.0.0
+      lerna info Assuming all packages changed
+
+      Changes:
+       - package-a: 0.0.0 => 3.3.3
+       - package-b: 0.0.0 => 3.3.3
+
+      lerna info auto-confirmed 
+      lerna info execute Skipping releases
+      lerna info git Pushing tags...
+      lerna success version finished
+
+    `);
+
+    const checkTagIsPresentLocally = await fixture.exec("git describe --abbrev=0");
+    expect(checkTagIsPresentLocally.combinedOutput).toMatchInlineSnapshot(`
+      v3.3.3
+
+    `);
+
+    const checkTagIsPresentOnRemote = await fixture.exec("git ls-remote origin refs/tags/v3.3.3");
+    expect(checkTagIsPresentOnRemote.combinedOutput).toMatchInlineSnapshot(`
+      {FULL_COMMIT_SHA}	refs/tags/v3.3.3
+
+    `);
+
+    const pnpmLockfileContent = await fixture.readWorkspaceFile("pnpm-lock.yaml");
+    const pnpmLockfileObject = <PnpmLockfile>load(pnpmLockfileContent);
+    expect(pnpmLockfileObject.importers).toMatchInlineSnapshot(`
+      Object {
+        .: Object {
+          devDependencies: Object {
+            lerna: 999.9.9-e2e.0,
+          },
+          specifiers: Object {
+            lerna: ^999.9.9-e2e.0,
+          },
+        },
+        packages/package-a: Object {
+          specifiers: Object {},
+        },
+        packages/package-b: Object {
+          dependencies: Object {
+            package-a: link:../package-a,
+          },
+          specifiers: Object {
+            package-a: ^3.3.3,
+          },
+        },
+      }
+    `);
+  });
 });
