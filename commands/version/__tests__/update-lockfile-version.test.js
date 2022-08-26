f5c84807b957079b6a6d771bb9ca327f88c15a8a
Austin Fahsl <austin@fahsl.io>
2022-08-26 09:32:27 -0400

    fix(version): only update existing lockfile deps (#3308)

--- commands/version/__tests__/update-lockfile-version.test.js
+++ commands/version/__tests__/update-lockfile-version.test.js
@@ -25,25 +25,6 @@ test("updateLockfileVersion with lockfile v1", async () => {
   expect(fs.readJSONSync(returnedLockfilePath)).toHaveProperty("version", "2.0.0");
 });
 
-test("updateLockfileVersion with lockfile v2", async () => {
-  const cwd = await initFixture("lockfile-leaf-v2");
-  const [pkg] = await getPackages(cwd);
-
-  pkg.version = "2.0.0";
-  pkg.dependencies["package-1"] = "^2.0.0";
-  pkg.devDependencies["package-2"] = "3.0.0";
-
-  const returnedLockfilePath = await updateLockfileVersion(pkg);
-
-  expect(returnedLockfilePath).toBe(path.join(pkg.location, "package-lock.json"));
-  expect(Array.from(loadJsonFile.registry.keys())).toStrictEqual(["/packages/package-1"]);
-  const updatedLockfile = fs.readJSONSync(returnedLockfilePath);
-  expect(updatedLockfile).toHaveProperty("version", "2.0.0");
-  expect(updatedLockfile).toHaveProperty(["packages", "", "dependencies", "package-1"], "^2.0.0");
-  expect(updatedLockfile).toHaveProperty(["packages", "", "dependencies", "tiny-tarball"], "^1.0.0");
-  expect(updatedLockfile).toHaveProperty(["packages", "", "devDependencies", "package-2"], "3.0.0");
-});
-
 test("updateLockfileVersion without sibling lockfile", async () => {
   const cwd = await initFixture("lifecycle", false);
   const [pkg] = await getPackages(cwd);
@@ -56,4 +37,45 @@ test("updateLockfileVersion without sibling lockfile", async () => {
 
   expect(returnedLockfilePath).toBeUndefined();
   expect(fs.pathExistsSync(path.join(pkg.location, "package-lock.json"))).toBe(false);
+});
+
+test("updateLockfileVersion with lockfile v2 - local dependency should be updated if already listed", async () => {
+  const cwd = await initFixture("lockfile-leaf-v2");
+  const [pkg] = await getPackages(cwd);
+
+  pkg.version = "2.0.0";
+  pkg.dependencies["package-2"] = "^2.0.0";
+  pkg.devDependencies["package-3"] = "3.0.0";
+
+  const returnedLockfilePath = await updateLockfileVersion(pkg);
+
+  expect(returnedLockfilePath).toBe(path.join(pkg.location, "package-lock.json"));
+  expect(Array.from(loadJsonFile.registry.keys())).toStrictEqual(["/packages/package-1"]);
+  const updatedLockfile = fs.readJSONSync(returnedLockfilePath);
+  expect(updatedLockfile).toHaveProperty("version", "2.0.0");
+  expect(updatedLockfile).toHaveProperty(["packages", "", "dependencies", "package-2"], "^2.0.0");
+  expect(updatedLockfile).toHaveProperty(["packages", "", "dependencies", "tiny-tarball"], "^1.0.0");
+  expect(updatedLockfile).toHaveProperty(["packages", "", "devDependencies", "package-3"], "3.0.0");
+});
+
+test("updateLockfileVersion with lockfile v2 - local dependency should not be added if not already listed", async () => {
+  const cwd = await initFixture("lockfile-v2-missing-local-deps");
+  const pkg2 = (await getPackages(cwd))[1];
+
+  pkg2.version = "2.0.0";
+  pkg2.dependencies["package-1"] = "^2.0.0";
+  pkg2.devDependencies["package-3"] = "^2.0.0";
+
+  const returnedLockfilePath = await updateLockfileVersion(pkg2);
+
+  expect(returnedLockfilePath).toBe(path.join(pkg2.location, "package-lock.json"));
+  const updatedLockfile = fs.readJSONSync(returnedLockfilePath);
+
+  expect(updatedLockfile).toHaveProperty("version", "2.0.0");
+
+  expect(updatedLockfile).toHaveProperty(["packages", "", "dependencies", "yargs"], "^6.6.0");
+  expect(updatedLockfile).not.toHaveProperty(["packages", "", "dependencies", "package-1"]);
+
+  expect(updatedLockfile).toHaveProperty(["packages", "", "devDependencies", "typescript"], "^4.0.0");
+  expect(updatedLockfile).not.toHaveProperty(["packages", "", "devDependencies", "package-3"]);
 });
