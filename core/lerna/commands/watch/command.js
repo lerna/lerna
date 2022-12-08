db813db48e8c6e7ae3c39b41ed85fb8e3a2ec057
Austin Fahsl <austin@fahsl.io>
2022-12-08 10:38:40 -0600

    feat(watch): wrap nx watch and add options support

--- core/lerna/commands/watch/command.js
+++ core/lerna/commands/watch/command.js
@@ -12,11 +12,28 @@ exports.command = "watch";
 exports.describe = "Runs a command whenever packages or their dependents change.";
 
 exports.builder = (yargs) => {
-  return filterOptions(
-    yargs.parserConfiguration({
+  yargs
+    .parserConfiguration({
       "populate--": true,
+      "strip-dashed": true,
     })
-  );
+    .option("includeGlobalWorkspaceFiles", {
+      type: "boolean",
+      description:
+        "Include global workspace files that are not part of a package. For example, the root eslint, or tsconfig file.",
+      alias: "g",
+    })
+    .option("callback", { type: "string", hidden: true })
+    .option("verbose", {
+      type: "boolean",
+      description: "Run watch mode in verbose mode, where commands are logged before execution.",
+    })
+    .middleware((args) => {
+      const { "--": underscore } = args;
+      args.callback = underscore?.[0];
+    }, true);
+
+  return filterOptions(yargs);
 };
 
 exports.handler = function handler(argv) {
--- core/lerna/commands/watch/index.js
+++ core/lerna/commands/watch/index.js
@@ -5,10 +5,7 @@
 const { Command } = require("@lerna/command");
 const { getFilteredPackages } = require("@lerna/filter-options");
 const { ValidationError } = require("@lerna/validation-error");
-const childProcess = require("@lerna/child-process");
-const pMap = require("p-map");
-const { daemonClient } = require("nx/src/daemon/client/client");
-const { exit } = require("process");
+const { watch } = require("../../../../../nx/build/packages/nx/src/command-line/watch");
 
 module.exports = factory;
 
@@ -22,73 +19,32 @@ class WatchCommand extends Command {
   }
 
   async initialize() {
-    const dashedArgs = this.options?.["--"] || [];
-    this.command = dashedArgs.shift();
-    this.args = dashedArgs;
-
-    if (!this.command) {
+    if (!this.options.callback) {
       throw new ValidationError("ENOCOMMAND", "A command to execute is required");
     }
-
-    // accessing properties of process.env can be expensive,
-    // so cache it here to reduce churn during tighter loops
-    this.env = Object.assign({}, process.env);
 
     this.filteredPackages = await getFilteredPackages(this.packageGraph, this.execOpts, this.options);
 
     this.count = this.filteredPackages.length;
     this.packagePlural = this.count === 1 ? "package" : "packages";
-    this.joinedCommand = [this.command].concat(this.args).join(" ");
   }
 
   async execute() {
     this.logger.info(
       "watch",
-      "Executing command in %d %s: %j",
+      "Executing command %j on changes in %d %s.",
+      this.options.callback,
       this.count,
-      this.packagePlural,
-      this.joinedCommand
+      this.packagePlural
     );
 
-    await pMap(this.filteredPackages, (pkg) => this.runCommandInPackageStreaming(pkg));
-
-    await daemonClient.registerFileWatcher(
-      {
-        watchGlobalWorkspaceFiles: true,
-        watchProjects: this.filteredPackages?.map((p) => p.name) || [],
-        // includeDependentProjects?: boolean
-      },
-      async (error, data) => {
-        if (error === "closed") {
-          // logger.error('...')
-          exit(1);
-        }
-        // otherwise, keep going, even if there is an error
-
-        // convert between projects <-> package name?
-        await pMap(data?.changedProjects || [], (pkg) => this.runCommandInPackageStreaming(pkg));
-      }
-    );
-
-    this.logger.success("watch", "Commands finished");
-  }
-
-  getOpts(pkg) {
-    return {
-      cwd: pkg.location,
-      shell: true,
-      extendEnv: false,
-      env: Object.assign({}, this.env, {
-        LERNA_PACKAGE_NAME: pkg.name,
-        LERNA_ROOT_PATH: this.project.rootPath,
-      }),
-      pkg,
-    };
-  }
-
-  runCommandInPackageStreaming(pkg) {
-    // runOne({ ... })
-    return childProcess.spawnStreaming(this.command, this.args, this.getOpts(pkg), pkg.name);
+    await watch({
+      callback: this.options.callback,
+      includeDependentProjects: false, // dependent projects are accounted for via lerna filter options
+      includeGlobalWorkspaceFiles: this.options.includeGlobalWorkspaceFiles,
+      projects: this.filteredPackages.map((p) => p.name),
+      verbose: this.options.verbose,
+    });
   }
 }
 
