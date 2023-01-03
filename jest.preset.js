a142ccafd33f1be53e1af1ff4bd141e29b0e98d1
“JamesHenry” <james@henry.sc>
2023-01-03 12:27:36 +0000

    wip

--- jest.preset.js
+++ jest.preset.js
@@ -1,4 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies, node/no-unpublished-require
 const nxPreset = require("@nrwl/jest/preset").default;
 
-module.exports = { ...nxPreset, modulePathIgnorePatterns: ["/__fixtures__/"] };
+module.exports = {
+  ...nxPreset,
+  clearMocks: true,
+  modulePathIgnorePatterns: ["/__fixtures__/"],
+};
