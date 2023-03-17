7963db9c4a236c0c47a7937628a4c71de618e01d
“JamesHenry” <james@henry.sc>
2023-03-17 18:25:43 +0400

    feat: add @lerna/legacy-package-management package
    This package contains the code for the legacy package-management concerns: add, bootstrap and link

---
+++ packages/legacy-package-management/README.md
@@ -0,0 +1,7 @@
+# legacy-package-management
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build legacy-package-management` to build the library.
---
+++ packages/legacy-package-management/src/index.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @nrwl/nx/enforce-module-boundaries */
+
+export * as addCmd from "@lerna/commands/add/command";
+export * as bootstrapCmd from "@lerna/commands/bootstrap/command";
+export * as linkCmd from "@lerna/commands/link/command";
