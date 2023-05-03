e1295038867677fecb517667350dd14bc060cdec
Austin Fahsl <austin@fahsl.io>
2023-05-03 12:37:08 -0600

    chore: add tests for getPackageManifestPath

---
+++ libs/core/src/lib/get-package-manifest-path.spec.ts
@@ -0,0 +1,42 @@
+import { getPackageManifestPath } from "./get-package-manifest-path";
+import { projectNode } from "./test-helpers/create-project-graph";
+
+describe("getPackageManifestPath", () => {
+  it("returns package.json path", () => {
+    const node = projectNode({
+      name: "package-1",
+      data: {
+        root: "/root/packages/package-1",
+        files: [
+          {
+            file: "/root/packages/package-1/index.js",
+            hash: "",
+          },
+          {
+            file: "/root/packages/package-1/package.json",
+            hash: "",
+          },
+        ],
+      },
+    });
+
+    expect(getPackageManifestPath(node)).toEqual("/root/packages/package-1/package.json");
+  });
+
+  it("returns undefined when no package.json exists", () => {
+    const node = projectNode({
+      name: "package-1",
+      data: {
+        root: "/root/packages/package-1",
+        files: [
+          {
+            file: "/root/packages/package-1/index.js",
+            hash: "",
+          },
+        ],
+      },
+    });
+
+    expect(getPackageManifestPath(node)).toBeUndefined();
+  });
+});
