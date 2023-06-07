e7e0489725038eb39973c2e908f688549cce2bac
James Henry <james@henry.sc>
2023-06-07 13:58:52 +0400

    chore: rework publish directory and assets (#3711)

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -3,22 +3,22 @@ export { addDependents } from "./lib/add-dependents";
 export { checkWorkingTree, throwIfUncommitted } from "./lib/check-working-tree";
 export * from "./lib/cli";
 export {
-  collectProjects,
-  collectProjectUpdates,
   ProjectCollectorOptions,
   ProjectUpdateCollectorOptions,
+  collectProjectUpdates,
+  collectProjects,
 } from "./lib/collect-updates";
 export { Command, PreInitializedProjectData } from "./lib/command";
 export { isGitInitialized } from "./lib/command/is-git-initialized";
 export { applyBuildMetadata, recommendVersion, updateChangelog } from "./lib/conventional-commits";
 export { describeRef, describeRefSync } from "./lib/describe-ref";
-export { filterOptions, FilterOptions } from "./lib/filter-options";
+export { FilterOptions, filterOptions } from "./lib/filter-options";
 export { filterProjects } from "./lib/filter-projects";
 export { getPackageManifestPath } from "./lib/get-package-manifest-path";
 export * from "./lib/get-packages-for-option";
 export { hasNpmVersion } from "./lib/has-npm-version";
 export { listableFormatProjects } from "./lib/listable-format-projects";
-export { listableOptions, ListableOptions } from "./lib/listable-options";
+export { ListableOptions, listableOptions } from "./lib/listable-options";
 export { logPacked } from "./lib/log-packed";
 export { Conf } from "./lib/npm-conf/conf";
 export { npmInstall, npmInstallDependencies } from "./lib/npm-install";
@@ -26,17 +26,17 @@ export { npmPublish } from "./lib/npm-publish";
 export { npmRunScript, npmRunScriptStreaming } from "./lib/npm-run-script";
 export { getOneTimePassword } from "./lib/otplease";
 export { output } from "./lib/output";
-export { packDirectory, Packed } from "./lib/pack-directory";
-export { ExtendedNpaResult, Package, RawManifest } from "./lib/package";
+export { Packed, packDirectory } from "./lib/pack-directory";
+export { AssetDefinition, ExtendedNpaResult, Package, RawManifest } from "./lib/package";
 export { prereleaseIdFromVersion } from "./lib/prerelease-id-from-version";
-export { generateProfileOutputPath, Profiler } from "./lib/profiler";
-export { CommandConfigOptions, Project, getPackages, LernaConfig } from "./lib/project";
+export { Profiler, generateProfileOutputPath } from "./lib/profiler";
+export { CommandConfigOptions, LernaConfig, Project, getPackages } from "./lib/project";
 export {
-  getPackage,
-  isExternalNpmDependency,
   ProjectGraphProjectNodeWithPackage,
   ProjectGraphWithPackages,
   ProjectGraphWorkspacePackageDependency,
+  getPackage,
+  isExternalNpmDependency,
 } from "./lib/project-graph-with-packages";
 export { promptConfirmation, promptSelectOne, promptTextInput } from "./lib/prompt";
 export { pulseTillDone } from "./lib/pulse-till-done";
