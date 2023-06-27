5fcf94e5718507dfea8821f806041ca41d8905a8
CSchulz <CSchulz@users.noreply.github.com>
2023-06-27 17:19:29 +0200

    fix: fix strict-ssl mapping for node-fetch-registry during unpublished projects lookup (#3747)

--- libs/commands/publish/src/lib/get-projects-with-unpublished-packages.ts
+++ libs/commands/publish/src/lib/get-projects-with-unpublished-packages.ts
@@ -15,6 +15,8 @@ export async function getProjectsWithUnpublishedPackages(
 
   const mapper = (node: ProjectGraphProjectNodeWithPackage) => {
     const pkg = getPackage(node);
+    // libnpmpublish / npm-registry-fetch check strictSSL rather than strict-ssl
+    opts["strictSSL"] = opts["strict-ssl"];
     return pacote.packument(pkg.name, opts).then(
       (packument) => {
         if (packument.versions === undefined || packument.versions[pkg.version] === undefined) {
