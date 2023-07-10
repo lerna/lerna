c3f3110023dc597391b7baaba5c0924871bef2c5
Austin Fahsl <austin@fahsl.io>
2023-07-10 12:50:57 -0600

    chore: bump Nx version to 16.5.1

--- libs/core/src/lib/command/detect-projects.ts
+++ libs/core/src/lib/command/detect-projects.ts
@@ -7,24 +7,7 @@ export async function detectProjects(packageConfigs: string[]): Promise<{
   projectFileMap: ProjectFileMap;
 }> {
   const _projectGraph = await createProjectGraphAsync();
-
-  let projectFileMap: ProjectFileMap;
-
-  // if we are using Nx >= 16.3.1, we can use the helper function to create the file map
-  // otherwise, we need to use the old "files" property on the node data
-  if (createProjectFileMapUsingProjectGraph) {
-    projectFileMap = await createProjectFileMapUsingProjectGraph(_projectGraph);
-  } else {
-    projectFileMap = Object.entries(_projectGraph.nodes).reduce(
-      (map, [key, node]) => ({
-        ...map,
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        [key]: (node.data as any).files,
-      }),
-      {}
-    );
-  }
-
+  const projectFileMap = await createProjectFileMapUsingProjectGraph(_projectGraph);
   const projectGraph = await createProjectGraphWithPackages(_projectGraph, projectFileMap, packageConfigs);
 
   return {
