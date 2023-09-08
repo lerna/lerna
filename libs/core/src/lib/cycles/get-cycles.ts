070498e68f0718337fb6d35648999343cff9bc39
Austin Fahsl <austin@fahsl.io>
2023-09-08 10:18:15 -0600

    fix(core): only traverse cycles included in project filters

--- libs/core/src/lib/cycles/get-cycles.ts
+++ libs/core/src/lib/cycles/get-cycles.ts
@@ -1,11 +1,9 @@
-import { ProjectGraphDependency } from "@nx/devkit";
-
 /**
  * Get all cycles within the given dependencies.
  * @param dependencies project dependencies grouped by source
  * @returns a list of cycles, where each cycle is a list of project names
  */
-export function getCycles(dependencies: Record<string, ProjectGraphDependency[]>): string[][] {
+export function getCycles(dependencies: Record<string, Set<string>>): string[][] {
   const cycles: string[][] = [];
   const visited: Set<string> = new Set();
 
@@ -14,11 +12,11 @@ export function getCycles(dependencies: Record<string, ProjectGraphDependency[]>
     path.push(next);
 
     for (const dep of dependencies[next] || []) {
-      if (path.includes(dep.target)) {
-        const cycle = path.slice(path.indexOf(dep.target));
+      if (path.includes(dep)) {
+        const cycle = path.slice(path.indexOf(dep));
         cycles.push(cycle);
-      } else if (!visited.has(dep.target)) {
-        dfs(dep.target, path);
+      } else if (!visited.has(dep)) {
+        dfs(dep, path);
       }
     }
 
