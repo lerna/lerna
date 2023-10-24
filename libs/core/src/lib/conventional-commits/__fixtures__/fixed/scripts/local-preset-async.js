bb5e7d7a35de6c801943c8349832558f1e6adb50
amorscher <ambros.morscher@bachmann.info>
2023-10-24 21:02:51 +0200

    fix(version): support changelog-presets using async factory funcs (#3873)

---
+++ libs/core/src/lib/conventional-commits/__fixtures__/fixed/scripts/local-preset-async.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// https://github.com/conventional-changelog/conventional-changelog/blob/b516084ef6a725197f148236c0ddbfae7ffe3e6f/packages/conventional-changelog-angular/conventional-recommended-bump.js
+const parserOpts = require("./parser-opts");
+const writerOpts = require("./writer-opts");
+const whatBump = require("./what-bump");
+
+async function createPreset(config) {
+  return {
+    conventionalChangelog: {
+      parserOpts,
+      writerOpts,
+    },
+    recommendedBumpOpts: {
+      parserOpts,
+      whatBump,
+    },
+  };
+}
+module.exports = createPreset;
