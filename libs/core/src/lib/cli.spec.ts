beca1b98cb9c7dab63b4d6d6193ae811db63b4a7
amorscher <am.morscher@gmail.com>
2023-11-17 20:42:39 +0100

    chore(deps): update yargs and yargs-parser to latest
     - enables async handler functions for commands

--- libs/core/src/lib/cli.spec.ts
+++ libs/core/src/lib/cli.spec.ts
@@ -12,20 +12,12 @@ function prepare(cwd: any) {
 }
 
 async function parse(
-  instance: { parse: (arg0: any, arg1: (exitError: any, argv: any, output: any) => void) => void },
+  instance: {
+    parse: (arg0: any, context: any) => void;
+  },
   args: string[]
 ): Promise<any> {
-  return new Promise((resolve, reject) => {
-    instance.parse(args, (exitError: any, argv: any, output: any) => {
-      if (exitError) {
-        // parity with synchronous errors
-        Object.assign(exitError, { argv, output });
-        reject(exitError);
-      } else {
-        resolve({ exitError, argv, output });
-      }
-    });
-  });
+  return instance.parse(args, {});
 }
 
 describe("core-cli", () => {
@@ -40,7 +32,7 @@ describe("core-cli", () => {
 
     cli.command("test-cmd", "will pass");
 
-    const { argv } = await parse(cli, [
+    await parse(cli, [
       "test-cmd",
       "--loglevel=warn",
       "--concurrency=10",
@@ -49,7 +41,7 @@ describe("core-cli", () => {
       "--max-buffer=1024",
     ]);
 
-    expect(argv).toMatchObject({
+    expect(cli.argv).toMatchObject({
       loglevel: "warn",
       concurrency: 10,
       progress: false,
@@ -60,9 +52,11 @@ describe("core-cli", () => {
 
   it("demands a command", async () => {
     const cli = prepare(cwd);
-    const cmd = parse(cli, ["--loglevel", "silent"]);
+    await parse(cli, ["--loglevel", "silent"]);
 
-    await expect(cmd).rejects.toThrow("A command is required.");
+    expect(loggingOutput("error")).toEqual([
+      "A command is required. Pass --help to see all available commands and options.",
+    ]);
   });
 
   it("recommends commands", async () => {
@@ -70,13 +64,14 @@ describe("core-cli", () => {
 
     cli.command("you", "shall not pass");
 
-    const cmd = parse(cli, ["yooou"]);
-    await expect(cmd).rejects.toThrow("Unknown argument: yooou");
+    await parse(cli, ["yooou"]);
+    //await expect(cmd).rejects.toThrow("Unknown argument: yooou");
 
-    const [unknownCmd, didYouMean] = loggingOutput("error");
+    const [unknownCmd, didYouMean, unkownArgument] = loggingOutput("error");
 
     expect(unknownCmd).toBe('Unknown command "yooou"');
     expect(didYouMean).toBe("Did you mean you?");
+    expect(unkownArgument).toBe("Unknown argument: yooou");
   });
 
   it("does not re-log ValidationError messages", async () => {
@@ -99,8 +94,8 @@ describe("core-cli", () => {
       throw new ValidationError("test", "...boom");
     });
 
-    // paradoxically, this does NOT reject...
-    await parse(cli, ["boom"]);
+    const cmd = parse(cli, ["boom"]);
+    await expect(cmd).rejects.toThrow("...boom");
 
     expect(loggingOutput("error")).toEqual(["...boom"]);
   });
@@ -108,7 +103,7 @@ describe("core-cli", () => {
   it("does not log errors with a pkg property", async () => {
     const cli = prepare(cwd);
 
-    cli.command("run", "a package error", {}, () => {
+    cli.command("run", "a package error", {}, async () => {
       const err = new Error("oops") as any;
       err.pkg = {}; // actual content doesn't matter here
       throw err;
@@ -129,8 +124,8 @@ describe("core-cli", () => {
       throw err;
     });
 
-    // paradoxically, this does NOT reject...
-    await parse(cli, ["handler"]);
+    const cmd = parse(cli, ["handler"]);
+    await expect(cmd).rejects.toThrow("yikes");
 
     expect(loggingOutput("error")).toEqual(["yikes"]);
     expect(spy).toHaveBeenLastCalledWith(
@@ -151,8 +146,8 @@ describe("core-cli", () => {
       throw err;
     });
 
-    // paradoxically, this does NOT reject...
-    await parse(cli, ["explicit"]);
+    const cmd = parse(cli, ["explicit"]);
+    await expect(cmd).rejects.toThrow("fancy fancy");
 
     expect(spy).toHaveBeenLastCalledWith(
       127,
