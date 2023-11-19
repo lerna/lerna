08c8b7537c49853ffc8a9fc3a79e18f335e7ccec
amorscher <am.morscher@gmail.com>
2023-11-19 01:53:43 +0100

    adapt test cli to yargs promise API

--- libs/test-helpers/src/lib/cli.ts
+++ libs/test-helpers/src/lib/cli.ts
@@ -3,6 +3,8 @@ import { resetWorkspaceContext, setupWorkspaceContext } from "nx/src/utils/works
 import { setWorkspaceRoot } from "nx/src/utils/workspace-root";
 import path, { join } from "path";
 import yargs from "yargs";
+import { lernaCLI } from "@lerna/core";
+import { strict } from "assert";
 
 /**
  * A higher-order function to help with passing _actual_ yargs-parsed argv
@@ -15,7 +17,6 @@ export function commandRunner(commandModule: yargs.CommandModule) {
   const cmd = commandModule.command.split(" ")[0];
 
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { lernaCLI } = require("@lerna/core");
 
   // prime the pump so slow-as-molasses CI doesn't fail with delayed require()
   // TODO: refactor to address type issues
@@ -32,60 +33,77 @@ export function commandRunner(commandModule: yargs.CommandModule) {
       .wrap(null)
       .command(commandModule);
 
-    return (...args: any) =>
+    return (...args: any) => {
       // eslint-disable-next-line no-async-promise-executor
-      new Promise(async (resolve, reject) => {
-        // We always need fresh copies of the graph in the unit test fixtures
-        process.env.NX_DAEMON = "false";
-        process.env.NX_CACHE_PROJECT_GRAPH = "false";
 
-        // Update the global workspaceRoot to the current test's cwd
-        setWorkspaceRoot(cwd);
-        // The environment variable is needed in order to influence the graph creation within lerna itself
-        process.env.NX_WORKSPACE_ROOT_PATH = cwd;
+      // We always need fresh copies of the graph in the unit test fixtures
+      process.env.NX_DAEMON = "false";
+      process.env.NX_CACHE_PROJECT_GRAPH = "false";
 
-        // Reset the Nx file hasher in order to respect the newly set workspaceRoot
-        resetWorkspaceContext();
-        setupWorkspaceContext(cwd);
+      // Update the global workspaceRoot to the current test's cwd
+      setWorkspaceRoot(cwd);
+      // The environment variable is needed in order to influence the graph creation within lerna itself
+      process.env.NX_WORKSPACE_ROOT_PATH = cwd;
 
-        const yargsMeta = {};
+      // Reset the Nx file hasher in order to respect the newly set workspaceRoot
+      resetWorkspaceContext();
+      setupWorkspaceContext(cwd);
 
-        const context = {
-          cwd,
-          lernaVersion: "__TEST_VERSION__",
-          loglevel: "silent",
-          progress: false,
-          onResolved: (result: any) => {
-            // success resolves the result, if any, returned from execute()
-            resolve(Object.assign({}, result, yargsMeta));
-          },
-          onRejected: (result: any) => {
-            Object.assign(result, yargsMeta);
-            // tests expect errors thrown to indicate failure,
-            // _not_ just non-zero exitCode
-            reject(result);
-          },
-        };
+      const yargsMeta: { error?: any; exitCode?: number } = {};
+      let realResult: any = {};
+      let rejectedResult: any = undefined;
+      const context = {
+        cwd,
+        lernaVersion: "__TEST_VERSION__",
+        loglevel: "silent",
+        progress: false,
+        onResolved: (res: any) => {
+          realResult = Object.assign({}, res, yargsMeta);
+        },
+        onRejected: (result: any) => {
+          Object.assign(result, yargsMeta);
+          // tests expect errors thrown to indicate failure,
+          // _not_ just non-zero exitCode
+          rejectedResult = result;
+        },
+      };
 
-        const parseFn = (yargsError: any, parsedArgv: any, yargsOutput: any) => {
-          // this is synchronous, before the async handlers resolve
-          Object.assign(yargsMeta, { parsedArgv, yargsOutput });
-        };
+      const parseFn = (yargsError: any, parsedArgv: any, yargsOutput: any) => {
+        // this is synchronous, before the async handlers resolve
+        Object.assign(yargsMeta, { parsedArgv, yargsOutput });
+      };
 
-        cli
+      // eslint-disable-next-line no-async-promise-executor
+      return new Promise((resolve, reject) => {
+        const result = cli
           .fail((msg: string | undefined, err: Error) => {
             // since yargs 10.1.0, this is the only way to catch handler rejection
             // _and_ yargs validation exceptions when using async command handlers
             const actual = err || new Error(msg);
-            // backfill exitCode for test convenience
-            // TODO: refactor to address type issues
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
-            yargsMeta.exitCode = "exitCode" in actual ? actual.exitCode : 1;
-            context.onRejected(actual);
+            yargsMeta.error = actual;
+            // ok lets keep the exit code here and always have one
+            if (!(actual as any).exitCode) {
+              (actual as any).exitCode = 1;
+            }
           })
           .parse([cmd, ...args], context, parseFn);
+        Promise.resolve(result)
+          .then(() => {
+            if (yargsMeta.error) {
+              reject(yargsMeta.error);
+              return;
+            }
+            if (rejectedResult) {
+              reject(rejectedResult);
+              return;
+            }
+            resolve(realResult);
+          })
+          .catch((e) => reject(e));
       });
+    };
   };
 }
 
