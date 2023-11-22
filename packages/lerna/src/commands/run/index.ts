4445fce9f9c897623451ae49a5e2bbd2f4d6e590
amorscher <ambros.morscher@bachmann.info>
2023-11-22 01:29:05 +0100

    chore(run): enable strict type checking (#3907)

--- packages/lerna/src/commands/run/index.ts
+++ packages/lerna/src/commands/run/index.ts
@@ -1,5 +1,1 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const runIndex = require("@lerna/commands/run");
-
-module.exports = runIndex;
-module.exports.RunCommand = runIndex.RunCommand;
+export * from "@lerna/commands/run";
