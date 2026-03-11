b2642e35143c59d7a55074fe2bf279a84057d65f
AI-JamesHenry <266146693+AI-JamesHenry@users.noreply.github.com>
2026-03-11 21:34:36 +0400

    test(core): revert to tar (minified bundle) to reproduce #4303 in CI
    Temporarily reverting tar/raw back to tar to prove the minified bundle
    drops files when glob patterns are used in package.json files field.
    This commit should FAIL the pack-directory-glob-files test in CI.

--- libs/core/src/lib/pack-directory.ts
+++ libs/core/src/lib/pack-directory.ts
@@ -2,9 +2,7 @@ import Arborist from "@npmcli/arborist";
 import packlist from "npm-packlist";
 import path from "path";
 import { IntegrityMap } from "ssri";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - use non-minified tar to avoid bundle bug in tar@7.5.9+ (index.min.js)
-import * as tar from "tar/raw";
+import * as tar from "tar";
 import { getPacked } from "./get-packed";
 import log from "./npmlog";
 import { Package } from "./package";
